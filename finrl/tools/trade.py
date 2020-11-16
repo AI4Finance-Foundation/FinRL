d2630781ae692e91a6858e489f878b261236312c
Bruce Yang <hongyang.yang@columbia.edu>
2020-11-16 17:38:35 -0500

    Create trade.py

---
+++ finrl/tools/trade.py
@@ -0,0 +1,12 @@
+from time import time
+
+
+def get_buy_and_hold_sharpe(test):
+    test['daily_return']=test['close'].pct_change(1)
+    sharpe = (252**0.5)*test['daily_return'].mean()/ \
+    test['daily_return'].std()
+    annual_return = ((test['daily_return'].mean()+1)**252-1)*100
+    print("annual return: ", annual_return)
+
+    print("sharpe ratio: ", sharpe)
+    #return sharpe
