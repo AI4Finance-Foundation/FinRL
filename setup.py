65f6759272f661a04b40c09a20f95be3f5e2eed2
Bruce Yang <hongyang.yang@columbia.edu>
2020-11-21 07:37:14 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     packages = find_packages(),
     install_requires=REQUIRES,
     dependency_links=[
-        'git+https://github.com/quantopian/pyfolio.git#egg=pyfolio'
+        'git+https://github.com/quantopian/pyfolio.git#egg=pyfolio-0.9.2'
     ],
     description = "FinRL library, a Deep Reinforcement Learning library designed specifically for automated stock trading.",
     classifiers = [
