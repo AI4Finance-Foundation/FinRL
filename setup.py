96b02f07c0fd750da5e39c7b226d28e9911a4a3a
Bruce Yang <hongyang.yang@columbia.edu>
2020-11-21 07:27:05 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,6 +24,9 @@ setup(
     license = "MIT" ,
     packages = find_packages(),
     install_requires=REQUIRES,
+    dependency_links=[
+        'git+https://github.com/quantopian/pyfolio.git#egg=pyfolio'
+    ],
     description = "FinRL library, a Deep Reinforcement Learning library designed specifically for automated stock trading.",
     classifiers = [
         # Trove classifiers
