15800c2ecb0c6c6bab9593550bfacc540ced77f3
Bruce Yang <hongyang.yang@columbia.edu>
2020-11-27 18:53:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ def main():
         os.makedirs("./" + config.DATA_SAVE_DIR)
     if not os.path.exists("./" + config.TRAINED_MODEL_DIR):
         os.makedirs("./" + config.TRAINED_MODEL_DIR)
+    if not os.path.exists("./" + config.TENSORBOARD_LOG_DIR):
+        os.makedirs("./" + config.TENSORBOARD_LOG_DIR)
     if not os.path.exists("./" + config.RESULTS_DIR):
         os.makedirs("./" + config.RESULTS_DIR)
 
