5bf21e1e0730fa23221cd1c79c0769a838181682
Spencer Romo <spencer.r.romo@gmailcom>
2020-12-31 15:03:51 -0600

    First commit * Refactor DRLAgent to allow instantiaton of model from single function * Refactor of model calling and storage * Change of configs to exclude training parameters * MODEL, MODEL_KWARGS, and NOISE menus (could be in config) * Change preprocessor to functional form.

--- finrl/preprocessing/preprocessors.py
+++ finrl/preprocessing/preprocessors.py
@@ -9,10 +9,6 @@ class FeatureEngineer:
 
     Attributes
     ----------
-        df: DataFrame
-            data downloaded from Yahoo API
-            7 columns: A date, open, high, low, close, volume and tick symbol
-            for the specified stock ticker
         use_technical_indicator : boolean
             we technical indicator or not
         tech_indicator_list : list
@@ -28,40 +24,32 @@ class FeatureEngineer:
         main method to do the feature engineering
 
     """
-    def __init__(self, 
-        df,
-        use_technical_indicator=True,
-        tech_indicator_list = config.TECHNICAL_INDICATORS_LIST,
-        use_turbulence=False,
-        user_defined_feature=False):
 
-        self.df = df
+    def __init__(
+        self,
+        use_technical_indicator=True,
+        tech_indicator_list=config.TECHNICAL_INDICATORS_LIST,
+        use_turbulence=False,
+        user_defined_feature=False,
+    ):
         self.use_technical_indicator = use_technical_indicator
         self.tech_indicator_list = tech_indicator_list
-        self.use_turbulence=use_turbulence
-        self.user_defined_feature=user_defined_feature
+        self.use_turbulence = use_turbulence
+        self.user_defined_feature = user_defined_feature
 
-        #type_list = self._get_type_list(5)
-        #self.__features = type_list
-        #self.__data_columns = config.DEFAULT_DATA_COLUMNS + self.__features
-
-
-    def preprocess_data(self):
+    def preprocess_data(self, df):
         """main method to do the feature engineering
         @:param config: source dataframe
         @:return: a DataMatrices object
         """
-        df = self.df.copy()
 
-        # add technical indicators
-        # stockstats require all 5 columns
-        if (self.use_technical_indicator==True):
+        if self.use_technical_indicator == True:
             # add technical indicators using stockstats
-            df=self.add_technical_indicator(df)
+            df = self.add_technical_indicator(df)
             print("Successfully added technical indicators")
 
         # add turbulence index for multiple stock
-        if self.use_turbulence==True:
+        if self.use_turbulence == True:
             df = self.add_turbulence(df)
             print("Successfully added turbulence index")
 
@@ -70,11 +58,9 @@ class FeatureEngineer:
             df = self.add_user_defined_feature(df)
             print("Successfully added user defined features")
 
-       
         # fill the missing values at the beginning and the end
-        df=df.fillna(method='bfill').fillna(method="ffill")
+        df = df.fillna(method="bfill").fillna(method="ffill")
         return df
-
 
     def add_technical_indicator(self, data):
         """
@@ -92,8 +78,10 @@ class FeatureEngineer:
             for i in range(len(unique_ticker)):
                 try:
                     temp_indicator = stock[stock.tic == unique_ticker[i]][indicator]
-                    temp_indicator= pd.DataFrame(temp_indicator)
-                    indicator_df = indicator_df.append(temp_indicator, ignore_index=True)
+                    temp_indicator = pd.DataFrame(temp_indicator)
+                    indicator_df = indicator_df.append(
+                        temp_indicator, ignore_index=True
+                    )
                 except Exception as e:
                     print(e)
             df[indicator] = indicator_df
@@ -104,15 +92,14 @@ class FeatureEngineer:
          add user defined features
         :param data: (df) pandas dataframe
         :return: (df) pandas dataframe
-        """          
+        """
         df = data.copy()
-        df['daily_return']=df.close.pct_change(1)
-        #df['return_lag_1']=df.close.pct_change(2)
-        #df['return_lag_2']=df.close.pct_change(3)
-        #df['return_lag_3']=df.close.pct_change(4)
-        #df['return_lag_4']=df.close.pct_change(5)
+        df["daily_return"] = df.close.pct_change(1)
+        # df['return_lag_1']=df.close.pct_change(2)
+        # df['return_lag_2']=df.close.pct_change(3)
+        # df['return_lag_3']=df.close.pct_change(4)
+        # df['return_lag_4']=df.close.pct_change(5)
         return df
-
 
     def add_turbulence(self, data):
         """
@@ -122,42 +109,45 @@ class FeatureEngineer:
         """
         df = data.copy()
         turbulence_index = self.calcualte_turbulence(df)
-        df = df.merge(turbulence_index, on='date')
-        df = df.sort_values(['date','tic']).reset_index(drop=True)
+        df = df.merge(turbulence_index, on="date")
+        df = df.sort_values(["date", "tic"]).reset_index(drop=True)
         return df
-
 
     def calcualte_turbulence(self, data):
         """calculate turbulence index based on dow 30"""
         # can add other market assets
         df = data.copy()
-        df_price_pivot=df.pivot(index='date', columns='tic', values='close')
+        df_price_pivot = df.pivot(index="date", columns="tic", values="close")
         unique_date = df.date.unique()
         # start after a year
         start = 252
-        turbulence_index = [0]*start
-        #turbulence_index = [0]
-        count=0
-        for i in range(start,len(unique_date)):
+        turbulence_index = [0] * start
+        # turbulence_index = [0]
+        count = 0
+        for i in range(start, len(unique_date)):
             current_price = df_price_pivot[df_price_pivot.index == unique_date[i]]
-            hist_price = df_price_pivot[[n in unique_date[0:i] for n in df_price_pivot.index ]]
+            hist_price = df_price_pivot[
+                [n in unique_date[0:i] for n in df_price_pivot.index]
+            ]
             cov_temp = hist_price.cov()
-            current_temp=(current_price - np.mean(hist_price,axis=0))
-            temp = current_temp.values.dot(np.linalg.inv(cov_temp)).dot(current_temp.values.T)
-            if temp>0:
-                count+=1
-                if count>2:
+            current_temp = current_price - np.mean(hist_price, axis=0)
+            temp = current_temp.values.dot(np.linalg.inv(cov_temp)).dot(
+                current_temp.values.T
+            )
+            if temp > 0:
+                count += 1
+                if count > 2:
                     turbulence_temp = temp[0][0]
                 else:
-                    #avoid large outlier because of the calculation just begins
-                    turbulence_temp=0
+                    # avoid large outlier because of the calculation just begins
+                    turbulence_temp = 0
             else:
-                turbulence_temp=0
+                turbulence_temp = 0
             turbulence_index.append(turbulence_temp)
-        
-        
-        turbulence_index = pd.DataFrame({'date':df_price_pivot.index,
-                                         'turbulence':turbulence_index})
+
+        turbulence_index = pd.DataFrame(
+            {"date": df_price_pivot.index, "turbulence": turbulence_index}
+        )
         return turbulence_index
 
     def _get_type_list(self, feature_number):
@@ -169,13 +159,13 @@ class FeatureEngineer:
             type_list = ["close"]
         elif feature_number == 2:
             type_list = ["close", "volume"]
-            #raise NotImplementedError("the feature volume is not supported currently")
+            # raise NotImplementedError("the feature volume is not supported currently")
         elif feature_number == 3:
             type_list = ["close", "high", "low"]
         elif feature_number == 4:
             type_list = ["close", "high", "low", "open"]
         elif feature_number == 5:
-            type_list = ["close", "high", "low", "open","volume"]  
+            type_list = ["close", "high", "low", "open", "volume"]
         else:
             raise ValueError("feature number could not be %s" % feature_number)
         return type_list
