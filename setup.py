1deda3a60da18d22278fcdaca5e5680ec7bcabee
Bruce Yang <hongyang.yang@columbia.edu>
2021-01-03 20:44:27 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ except:
 
 setup(
     name="finrl",
-    version="0.0.1",
+    version="0.0.2",
     include_package_data=True,
     author="Hongyang Yang, Xiaoyang Liu",
     author_email="hy2500@columbia.edu",
@@ -28,6 +28,13 @@ setup(
     + ["pyfolio @ git+https://github.com/quantopian/pyfolio.git#egg=pyfolio-0.9.2"],
     # dependency_links=['git+https://github.com/quantopian/pyfolio.git#egg=pyfolio-0.9.2'],
     description="FinRL library, a Deep Reinforcement Learning library designed specifically for automated stock trading.",
+    long_description = """finrl is a Python library for that facilitates beginners to expose themselves to quantitative finance 
+    and to develop their own trading strategies, it is developed by `AI4Finance`_. 
+    
+    FinRL has been developed under three primary principles: completeness, hands-on tutorial and reproducibility. 
+    
+    .. _AI4Finance: https://github.com/AI4Finance-LLC
+    """,
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
