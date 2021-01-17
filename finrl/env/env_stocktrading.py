1240aa909c83d3012c34493d4e9ac37e892cb439
Louis Owen <louisowen6@gmail.com>
2021-01-17 12:18:26 +0700

    env_update

--- finrl/env/env_stocktrading.py
+++ finrl/env/env_stocktrading.py
@@ -20,7 +20,8 @@ class StockTradingEnv(gym.Env):
                 stock_dim,
                 hmax,                
                 initial_amount,
-                transaction_cost_pct,
+                buy_cost_pct,
+                sell_cost_pct,
                 reward_scaling,
                 state_space,
                 action_space,
@@ -34,7 +35,8 @@ class StockTradingEnv(gym.Env):
         self.stock_dim = stock_dim
         self.hmax = hmax
         self.initial_amount = initial_amount
-        self.transaction_cost_pct =transaction_cost_pct
+        self.buy_cost_pct = buy_cost_pct
+        self.sell_cost_pct = sell_cost_pct
         self.reward_scaling = reward_scaling
         self.state_space = state_space
         self.action_space = action_space
@@ -81,47 +83,29 @@ class StockTradingEnv(gym.Env):
                 sell_num_shares = 0
                 pass
             return sell_num_shares
-
+            
         # perform sell action based on the sign of the action
         if self.turbulence_threshold is not None:
-            if self.turbulence>self.turbulence_threshold:
+            if self.turbulence>=self.turbulence_threshold:
                 # if turbulence goes over threshold, just clear out all positions 
                 if self.state[index+self.stock_dim+1] > 0:
+                    sell_num_shares = self.state[index+self.stock_dim+1]
+                    sell_amount = self.state[index+1]*sell_num_shares* (1- self.sell_cost_pct)
                     #update balance
-                    self.state[0] += self.state[index+1]*self.state[index+self.stock_dim+1]* \
-                                (1- self.transaction_cost_pct)
+                    self.state[0] += sell_amount
                     self.state[index+self.stock_dim+1] =0
                     self.cost += self.state[index+1]*self.state[index+self.stock_dim+1]* \
-                                self.transaction_cost_pct
+                                self.sell_cost_pct
                     self.trades+=1
                 else:
-<<<<<<< HEAD
-                    pass
-        else:
-            # perform sell action based on the sign of the action
-            if self.state[index+self.stock_dim+1] > 0:
-                #update balance
-                self.state[0] += \
-                self.state[index+1]*min(abs(action),self.state[index+self.stock_dim+1]) * \
-                (1- self.transaction_cost_pct)
-
-                self.state[index+self.stock_dim+1] -= min(abs(action), self.state[index+self.stock_dim+1])
-                self.cost +=self.state[index+1]*min(abs(action),self.state[index+self.stock_dim+1]) * \
-                self.transaction_cost_pct
-                self.trades+=1
-            else:
-                pass
-
-=======
                     sell_num_shares = 0
                     pass
             else:
                 sell_num_shares = _do_sell_normal()
         else:
             sell_num_shares = _do_sell_normal()
-            
+
         return sell_num_shares
->>>>>>> 43d67c3... update action state & bug fixing in _sell_stock
 
     
     def _buy_stock(self, index, action):
@@ -131,22 +115,27 @@ class StockTradingEnv(gym.Env):
             # print('available_amount:{}'.format(available_amount))
             
             #update balance
-            self.state[0] -= self.state[index+1]*min(available_amount, action)* \
-                              (1+ self.transaction_cost_pct)
+            buy_num_shares = min(available_amount, action)
+            buy_amount = self.state[index+1]* buy_num_shares * (1+ self.buy_cost_pct)
+            self.state[0] -= buy_amount
 
             self.state[index+self.stock_dim+1] += min(available_amount, action)
             
             self.cost+=self.state[index+1]*min(available_amount, action)* \
-                              self.transaction_cost_pct
+                              self.buy_cost_pct
             self.trades+=1
+
+            return buy_num_shares
         # perform buy action based on the sign of the action
         if self.turbulence_threshold is None:
-            _do_buy()
+            buy_num_shares = _do_buy()
         else:
             if self.turbulence< self.turbulence_threshold:
-                _do_buy()
+                buy_num_shares = _do_buy()
             else:
                 pass
+
+        return buy_num_shares
 
     def _make_plot(self):
         plt.plot(self.asset_memory,'r')
@@ -191,9 +180,8 @@ class StockTradingEnv(gym.Env):
 
         else:
 
-            actions = actions * self.hmax
-            self.actions_memory.append(actions)
-            #actions = (actions.astype(int))
+            actions = actions * self.hmax #actions initially is scaled between 0 to 1
+            actions = (actions.astype(int)) #convert into integer because we can't by fraction of shares
             if self.turbulence_threshold is not None:
                 if self.turbulence>=self.turbulence_threshold:
                     actions=np.array([-self.hmax]*self.stock_dim)
@@ -207,12 +195,17 @@ class StockTradingEnv(gym.Env):
             buy_index = argsort_actions[::-1][:np.where(actions > 0)[0].shape[0]]
 
             for index in sell_index:
-                # print('take sell action'.format(actions[index]))
-                self._sell_stock(index, actions[index])
+                # print(f"Num shares before: {self.state[index+self.stock_dim+1]}")
+                # print(f'take sell action before : {actions[index]}')
+                actions[index] = self._sell_stock(index, actions[index])
+                # print(f'take sell action after : {actions[index]}')
+                # print(f"Num shares after: {self.state[index+self.stock_dim+1]}")
 
             for index in buy_index:
                 # print('take buy action: {}'.format(actions[index]))
-                self._buy_stock(index, actions[index])
+                actions[index] = self._buy_stock(index, actions[index])
+
+            self.actions_memory.append(actions)
 
             self.day += 1
             self.data = self.df.loc[self.day,:]    
