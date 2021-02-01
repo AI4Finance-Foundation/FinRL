273bdd693bd61691c7ee604ac06badeb8c286061
Louis Owen <louisowen6@gmail.com>
2021-02-01 23:48:33 +0700

    add tests for shares_increment

--- tests/test_env/test_env_cashpenalty.py
+++ tests/test_env/test_env_cashpenalty.py
@@ -41,6 +41,30 @@ class TestStocktradingEnvCashpenalty(unittest.TestCase):
             self.assertEqual(init_amt, total_assets)
             self.assertEqual(i + 1, env.current_step)
 
+    def test_shares_increment(self):
+        # Prove that we can only buy multiplies of shares based on shares_increment parameter
+        init_amt = 1e6
+        hmax = 5_000
+        shares_increment = 10
+        env = StockTradingEnvCashpenalty(
+            df=self.df, initial_amount=init_amt, hmax=hmax,
+            cache_indicator_data=False,shares_increment=shares_increment
+        )
+        _ = env.reset()
+
+        actions = np.array([29.0,0.0])
+        next_state, _, _, _ = env.step(actions)
+        holdings = next_state[1 : 1 + len(self.ticker_list)]
+        self.assertEqual(holdings[0], 20.0)
+        self.assertEqual(holdings[1], 0.0)
+
+        actions = np.array([-12.0,0.0])
+        next_state, _, _, _ = env.step(actions)
+        holdings = next_state[1 : 1 + len(self.ticker_list)]
+        self.assertEqual(holdings[0], -10.0)
+        self.assertEqual(holdings[1], 0.0)
+
+
     def test_cost_penalties(self):
         # TODO: Requesting contributions!
         pass
