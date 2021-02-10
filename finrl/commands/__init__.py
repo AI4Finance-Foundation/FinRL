cdeb2a0639f94b099d3b0d43af1ac07ac4b56ede
Youbad <youbadawy@gmail.com>
2021-02-10 09:09:33 -0500

    List Markets, Pairs Crypto

--- finrl/commands/__init__.py
+++ finrl/commands/__init__.py
@@ -5,3 +5,5 @@ Contains all start-commands, subcommands and CLI Interface creation.
 """
 from finrl.commands.deploy_commands import start_create_userdir
 from finrl.commands.data_commands import start_download_cryptodata, start_download_stockdata
+from finrl.commands.list_commands import (start_list_exchanges, start_list_markets, 
+                                              start_list_timeframes, start_show_trades)
