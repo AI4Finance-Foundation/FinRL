27736f02bfe747158f93f307b25a0387ad1c4f26
Spencer Romo <spencer.r.romo@gmail.com>
2021-02-14 16:33:39 -0600

    Ledger working * Pure python implementation * Implement ability to observe long term shares * Ability to do computations correctly. Probably will break in training, but maybe it works fine?

--- finrl/env/accounting/ledger.py
+++ finrl/env/accounting/ledger.py
@@ -1,78 +1,97 @@
 import pandas as pd
 import numpy as np
 from copy import copy
-from pprint import pprint
 import datetime
 
-class Ledger():
-    
-    '''
-    This class allows for the buying and selling of assets, 
-    primarily now it will assist in determining tax implications of sales. 
-    
-    In the future, it could be used to track environment information in an enhanced way. 
-    '''
-    
-    def __init__(self, assets, df = None, tax_threshold_days=365):
+
+class Ledger:
+
+    """
+    This class allows for the buying and selling of assets,
+    primarily now it will assist in determining tax implications of sales.
+
+    In the future, it could be used to track environment information in an enhanced way.
+    """
+
+    def __init__(self, assets, df=None, tax_threshold_days=365):
         self.assets = assets
-        self.d = {a:{} for a in assets}
+        self.d = {a: {} for a in assets}
         self.tax_threshold_days = tax_threshold_days
-        
-        
+        self.dates = []
+
     def date_from_str(self, datestr):
         return pd.to_datetime(datestr, infer_datetime_format=True).date()
-    
+
     def date_to_str(self, date):
-        return str(date)    
-    
+        return str(date)
+
     def log_date(self, date, transactions):
-        
+
         if isinstance(date, datetime.date):
-            print("got here!")
             date = self.date_to_str(date)
-        
+
         assert isinstance(date, str)
+        if date not in self.dates:
+            self.dates.append(self.date_from_str(date))
         tax_d = {}
         for i, (a, t) in enumerate(zip(self.assets, transactions)):
             buys, sells = max(0, t), -min(0, t)
-            
+
             self.d[a][date] = {"buys": buys, "sells": sells, "tax_used": 0}
-            if sells > 0 : #consider order here
+            if sells > 0:  # consider order here
                 tax_d[i] = self.compute_tax_lots(a, sells, date)
         return tax_d
-                
+
+    def get_longterm_holdings(self):
+        # given all assets to date, figure out what the available long term and short term tax options are
+        l = [0 for _ in self.assets]
+        before_dates = [
+            x
+            for x in self.dates
+            if (self.dates[-1] - datetime.timedelta(self.tax_threshold_days)) > x
+        ]
+        for i, a in enumerate(self.assets):
+            for d in before_dates:
+                d = self.date_to_str(d)
+                l[i] += self.d[a][d]["buys"] - self.d[a][d]["tax_used"]
+        return l
+
     def compute_tax_lots(self, asset, sells, sell_date):
-#         print(f"computing tax lots")
         a_data = copy(self.d[asset])
         remaining_shares = sells
-        long_shares= 0
+        long_shares = 0
         short_shares = 0
-        #ok, let's loop here and use up shares oldest to youngest
+        # ok, let's loop here and use up shares oldest to youngest
         dates = sorted(a_data)
         i = 0
         while remaining_shares > 0:
             date = dates[i]
-            long_term = (self.date_from_str(sell_date)-datetime.timedelta(days = self.tax_threshold_days))>self.date_from_str(date)
-#             print(f"date: {date}, long term: {long_term}, remaining_shares: {remaining_shares}")
-                
+            long_term = (
+                self.date_from_str(sell_date)
+                - datetime.timedelta(days=self.tax_threshold_days)
+            ) > self.date_from_str(date)
+
             d = a_data[date]
-            avail_shares = d['buys'] - d['tax_used']
-            if avail_shares>0:
-                #let's consume these
-                if avail_shares<remaining_shares:
+            avail_shares = d["buys"] - d["tax_used"]
+            if avail_shares > 0:
+                # let's consume these
+                if avail_shares < remaining_shares:
                     shares_consumed = avail_shares
                 else:
                     shares_consumed = remaining_shares
-                    
-                remaining_shares-=shares_consumed
+
+                remaining_shares -= shares_consumed
                 if long_term:
-                    long_shares+=shares_consumed
+                    long_shares += shares_consumed
                 else:
-                    short_shares+=shares_consumed
-                
-                a_data[date]['tax_used'] = shares_consumed
-            i+=1
-                
+                    short_shares += shares_consumed
+
+                a_data[date]["tax_used"] = shares_consumed
+            i += 1
+
         self.d[asset] = a_data
-        return {"asset": asset, "long_term_shares": long_shares, "short_term_shares": short_shares}
-                
+        return {
+            "asset": asset,
+            "long_term_shares": long_shares,
+            "short_term_shares": short_shares,
+        }
--- tests/test_env/test_accounting/test_ledger.py
+++ tests/test_env/test_accounting/test_ledger.py
@@ -6,43 +6,70 @@ import pandas as pd
 import datetime
 
 
-
-
 class TestLedger(unittest.TestCase):
-
-
-
     def test_trivial(self):
         self.assertTrue(True)
 
     def test_instantation_and_insert(self):
         n = 12
-    
-        l = Ledger(assets = ["AAPL", "GOOG"], tax_threshold_days=7)
-        datelist = [d.date() for d in pd.date_range(datetime.datetime.today()-datetime.timedelta(days = n), periods=n)]
+
+        l = Ledger(assets=["AAPL", "GOOG"], tax_threshold_days=7)
+        datelist = [
+            d.date()
+            for d in pd.date_range(
+                datetime.datetime.today() - datetime.timedelta(days=n), periods=n
+            )
+        ]
         for d in datelist[:10]:
-            print(d)
-            _ = l.log_date(d, [1.0,1.0])
-        '''
+            _ = l.log_date(d, [1.0, 1.0])
+        """
         Since our tax horizon here is 7 days, we expect that the only long term shares
             are in the first 3 days of our 10. 
         So, selling 5 and 4 units respectively, we should get back 3 from long term for each sale here
-        '''
+        """
         tax_implications = l.log_date(str(datelist[10]), [-5, -4])
-        self.assertEqual(tax_implications[0]['long_term_shares'], 3)
-        self.assertEqual(tax_implications[0]['short_term_shares'],2)
-        self.assertEqual(tax_implications[0]['short_term_shares'] + tax_implications[0]['long_term_shares'], 5)
+        self.assertEqual(tax_implications[0]["long_term_shares"], 3)
+        self.assertEqual(tax_implications[0]["short_term_shares"], 2)
+        self.assertEqual(
+            tax_implications[0]["short_term_shares"]
+            + tax_implications[0]["long_term_shares"],
+            5,
+        )
         self.assertTrue(1 in tax_implications)
-        '''
+        """
         Since we have now exhausted our early purchases in terms of long term, we should get entirely short term shares here
 
-        '''
+        """
         trans2 = [-4, -4]
         tax_implications = l.log_date(str(datelist[11]), trans2)
         print(tax_implications)
         for i, v in enumerate(trans2):
-            self.assertEqual(tax_implications[i]['short_term_shares'], -v)
-        
+            self.assertEqual(tax_implications[i]["short_term_shares"], -v)
+
         print(tax_implications)
 
-    
+    def test_count_holdings(self):
+        n = 12
+
+        l = Ledger(assets=["AAPL", "GOOG"], tax_threshold_days=7)
+        datelist = [
+            d.date()
+            for d in pd.date_range(
+                datetime.datetime.today() - datetime.timedelta(days=n), periods=n
+            )
+        ]
+        for d in datelist[:10]:
+            _ = l.log_date(d, [1.0, 1.0])
+
+        results = l.get_longterm_holdings()
+        self.assertTrue(len(results) == 2)
+        self.assertEqual(results, [2.0, 2.0])
+        print(results)
+
+        # let's sell some stuff.
+        # sell 2 units each. But we advance a day, so we get one additional unit
+        # result shoudl be that after sale we have 1 long term tax unit and the rest short term.
+        tax_imps = l.log_date(datelist[-2], [-2, -2])
+        results = l.get_longterm_holdings()
+        self.assertEqual(results, [1.0, 1.0])
+        print(f"final results: {results}")
