87223f84cac6c84889748fc1559bda804d0ec754
Bruce Yang <hongyang.yang@columbia.edu>
2021-02-26 02:27:23 +0800

    Update backtest.py

--- finrl/trade/backtest.py
+++ finrl/trade/backtest.py
@@ -19,6 +19,14 @@ def get_daily_return(df, value_col_name="account_value"):
     df.index = df.index.tz_localize("UTC")
     return pd.Series(df["daily_return"], index=df.index)
 
+def convert_daily_return_to_pyfolio_ts(df):
+    strategy_ret= df.copy()
+    strategy_ret['date'] = pd.to_datetime(strategy_ret['date'])
+    strategy_ret.set_index('date', drop = False, inplace = True)
+    strategy_ret.index = strategy_ret.index.tz_localize('UTC')
+    del strategy_ret['date']
+    ts = pd.Series(strategy_ret['daily_return'].values, index=strategy_ret.index)
+    return ts
 
 def backtest_stats(account_value, value_col_name="account_value"):
     dr_test = get_daily_return(account_value, value_col_name=value_col_name)
