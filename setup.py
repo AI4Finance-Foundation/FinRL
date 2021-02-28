51cea1284eea48d7738886a1aa1bc3bd31c00986
Bruce Yang <hongyang.yang@columbia.edu>
2021-03-01 01:00:34 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ except:
 
 setup(
     name="finrl",
-    version="0.0.3",
+    version="0.3.0",
     include_package_data=True,
     author="Hongyang Yang, Xiaoyang Liu",
     author_email="hy2500@columbia.edu",
