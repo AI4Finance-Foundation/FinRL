2706d0910e9e627411c82ca618d5b6da3dec474d
brucehy <hongyang.yang@columbia.edu>
2021-08-15 18:10:49 +0800

    Update data_processor.py

--- finrl/neo_finrl/data_processor.py
+++ finrl/neo_finrl/data_processor.py
@@ -1,35 +1,37 @@
-from finrl.neo_finrl.data_processors.processor_alpaca import AlpacaEngineer as AE
-from finrl.neo_finrl.data_processors.processor_ccxt import CCXTEngineer as CE
-from finrl.neo_finrl.data_processors.processor_joinquant import JoinQuantEngineer as JE
-from finrl.neo_finrl.data_processors.processor_wrds import WrdsEngineer as WE
-from finrl.neo_finrl.data_processors.processor_yahoofinance import YahooFinanceEngineer as YE
+from neo_finrl.data_processors.processor_alpaca import AlpacaProcessor as Alpaca
+from neo_finrl.data_processors.processor_wrds import WrdsProcessor as Wrds
+from neo_finrl.data_processors.processor_yahoofinance import YahooFinanceProcessor as YahooFinance
 import pandas as pd
 import numpy as np
 
 class DataProcessor():
     def __init__(self, data_source, **kwargs):
         if data_source == 'alpaca':
+            
             try:
                 API_KEY= kwargs.get('API_KEY')
                 API_SECRET= kwargs.get('API_SECRET')
                 APCA_API_BASE_URL= kwargs.get('APCA_API_BASE_URL')
-                self.processor = AE(API_KEY, API_SECRET, APCA_API_BASE_URL)
-                print('alpaca successfully connect')
+                self.processor = Alpaca(API_KEY, API_SECRET, APCA_API_BASE_URL)
+                print('Alpaca successfully connected')
             except:
                 raise ValueError('Please input correct account info for alpaca!')
-        elif data_source == 'ccxt':
-            self.processor = CE
-        elif data_source == 'joinquant':
-            self.processor = JE
+                
         elif data_source == 'wrds':
-            self.processor = WE
+            self.processor = Wrds()
+            
         elif data_source == 'yahoofinance':
-            self.processor = YE
+            self.processor = YahooFinance()
+        
+        else:
+            raise ValueError('Data source input is NOT supported yet.')
     
-    def data_fetch(self,stock_list=['AAPL'], start_date='2021-05-10',
-                   end_date='2021-05-10',time_interval='1Min'):
-        df = self.processor.data_fetch(self, stock_list, start_date, end_date,
-                                       time_interval)
+    def download_data(self, ticker_list, start_date, end_date, 
+                      time_interval) -> pd.DataFrame:
+        df = self.processor.download_data(ticker_list = ticker_list, 
+                                          start_date = start_date, 
+                                          end_date = end_date,
+                                          time_interval = time_interval)
         return df
     
     def clean_data(self, df) -> pd.DataFrame:
@@ -37,20 +39,26 @@ class DataProcessor():
         
         return df
     
-    def add_technical_indicators(self, df, tech_indicator_list = [
-            'macd', 'boll_ub', 'boll_lb', 'rsi_30', 'dx_30',
-            'close_30_sma', 'close_60_sma']) -> pd.DataFrame:
+    def add_technical_indicator(self, df, tech_indicator_list) -> pd.DataFrame:
+        self.tech_indicator_list = tech_indicator_list
         df = self.processor.add_technical_indicator(df, tech_indicator_list)
         
         return df
     
-    def add_turbulence(self,df) -> pd.DataFrame:
+    def add_turbulence(self, df) -> pd.DataFrame:
         df = self.processor.add_turbulence(df)
         
         return df
     
-    def df_to_ary(self, df) -> np.array:
-        price_ary,tech_ary,turbulence_ary = self.df_to_ary(df)
+    def add_vix(self, df) -> pd.DataFrame:
+        df = self.processor.add_vix(df)
         
-        return price_ary,tech_ary,turbulence_ary
+        return df
+    
+    def df_to_array(self, df, if_vix) -> np.array:
+        price_array,tech_array,turbulence_array = self.processor.df_to_array(df,
+                                                self.tech_indicator_list,
+                                                if_vix)
+        
+        return price_array,tech_array,turbulence_array
         
