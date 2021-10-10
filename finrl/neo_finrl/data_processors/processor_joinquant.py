7195af6c0c08a0844a8c16607ad329639923447e
mingzhu <zhumingpassional@gmail.com>
2021-10-10 22:34:20 +0800

    add

--- finrl/neo_finrl/data_processors/processor_joinquant.py
+++ finrl/neo_finrl/data_processors/processor_joinquant.py
@@ -101,8 +101,8 @@ if __name__ == '__main__':
 
 
     e = JoinQuantEngineer()
-    username = ''
-    password = ''
+    username = 'xxx'
+    password = 'xxx'
     e.auth(username, password)
 
     trade_days = e.calc_trade_days_by_joinquant(TRADE_START_DATE, TRADE_END_DATE)
