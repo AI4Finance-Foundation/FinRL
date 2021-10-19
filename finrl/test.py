b2da36f9b6ab7fe6ea2f89d386dbf6fa0bb1ecd6
Bruce Yang <hongyang.yang@columbia.edu>
2021-10-19 23:00:17 +0800

    2021-10-20 test.py refactoring

--- finrl/test.py
+++ finrl/test.py
@@ -1,173 +1,121 @@
-#from elegantrl.agent import *
-#from elegantrl.run import *
-#import elegantrl
+import pandas as pd
+import numpy as np
+from elegantrl.agent import *
+from elegantrl.run import *
 import torch 
-#from neo_finrl.data_processor import DataProcessor
+import ray
+from finrl.apps import config
+# import DRL agents
+from finrl.drl_agents.stablebaselines3.models import DRLAgent as DRLAgent_sb3
+from finrl.drl_agents.rllib.models import DRLAgent as DRLAgent_rllib
+from finrl.drl_agents.elegantrl.models import DRLAgent as DRLAgent_erl
+# import data processor
+from finrl.neo_finrl.data_processor import DataProcessor
 
 def test(start_date, end_date, ticker_list, data_source, time_interval, 
-         technical_indicator_list, drl_lib, env, agent, if_vix = True,
+         technical_indicator_list, drl_lib, env, model_name, if_vix = True,
          **kwargs):
     #fetch data
     DP = DataProcessor(data_source, **kwargs)
     data = DP.download_data(ticker_list, start_date, end_date, time_interval)
     data = DP.clean_data(data)
     data = DP.add_technical_indicator(data, technical_indicator_list)
+    
     if if_vix:
         data = DP.add_vix(data)
-    price_array, tech_array, risk_array = DP.df_to_array(data, if_vix)
+    price_array, tech_array, turbulence_array = DP.df_to_array(data, if_vix)
     
     env_config = {'price_array':price_array,
             'tech_array':tech_array,
-            'risk_array':risk_array,
+            'turbulence_array':turbulence_array,
             'if_train':False}
     env_instance = env(config=env_config)
-    
+
+    #load elegantrl needs state dim, action dim and net dim
     net_dimension = kwargs.get('net_dimension', 2**7)
-    cwd = kwargs.get('cwd','./'+str(agent))
+    cwd = kwargs.get('cwd','./'+str(model_name))
+    print("price_array: ",len(price_array))
 
-    #test on elegantrl
     if drl_lib == 'elegantrl':
-        
-        #select agent
-        if agent == 'ppo':
-            args = Arguments(if_on_policy=True)
-            args.agent = AgentPPO()
-            args.env = env_instance
-            args.agent.if_use_cri_target = True
-        else:
-            raise ValueError('Invalid agent input or the agent input is not \
-                             supported yet.')
-        
-        #load agent
-        try:
-            state_dim = env_instance.state_dim
-            action_dim = env_instance.action_dim
-    
-            agent = args.agent
-            net_dim = net_dimension
-    
-            agent.init(net_dim, state_dim, action_dim)
-            agent.save_or_load_agent(cwd=cwd, if_save=False)
-            act = agent.act
-            device = agent.device
-    
-        except:
-            raise ValueError('Fail to load agent!')
-        
-        #test on the testing env
-        _torch = torch
-        state = env_instance.reset()
-        episode_returns = list()  # the cumulative_return / initial_account
-        with _torch.no_grad():
-            for i in range(env_instance.max_step):
-                s_tensor = _torch.as_tensor((state,), device=device)
-                a_tensor = act(s_tensor)  # action_tanh = act.forward()
-                action = a_tensor.detach().cpu().numpy()[0]  # not need detach(), because with torch.no_grad() outside
-                state, reward, done, _ = env_instance.step(action)
+        episode_total_assets = DRLAgent_erl.DRL_prediction(model_name=model_name,
+                                            cwd=cwd,
+                                            net_dimension=net_dimension,
+                                            environment=env_instance)
 
-                total_asset = env_instance.amount + (env_instance.price_ary[env_instance.day] * env_instance.stocks).sum()
-                episode_return = total_asset / env_instance.initial_total_asset
-                episode_returns.append(episode_return)
-                if done:
-                    break
-        print('Test Finished!')
-        #return episode returns on testing data
-        return episode_returns
+        return episode_total_assets
     
-    #test using rllib
     elif drl_lib == 'rllib':
         #load agent
-        from ray.rllib.agents import ppo
-        from ray.rllib.agents.ppo.ppo import PPOTrainer
-        config = ppo.DEFAULT_CONFIG.copy()
-        config['env'] = env
-        config["log_level"] = "WARN"
-        config['env_config'] = {'price_array':price_array,
-                                'tech_array':tech_array,
-                                'risk_array':risk_array,
-                                'if_train':False}
-        
-        trainer = PPOTrainer(env=env, config=config)
-        try:
-            trainer.restore(cwd)
-            print("Restoring from checkpoint path", cwd)
-        except:
-            raise ValueError('Fail to load agent!')
-        
-        #test on the testing env
-        state = env_instance.reset()
-        episode_returns = list()  # the cumulative_return / initial_account
-        done = False
-        while not done:
-            action = trainer.compute_single_action(state)
-            state, reward, done, _ = env_instance.step(action)
-    
-            total_asset = env_instance.amount + (env_instance.price_ary[env_instance.day] * env_instance.stocks).sum()
-            episode_return = total_asset / env_instance.initial_total_asset
-            episode_returns.append(episode_return)
-        print('episode return: ' + str(episode_return))
-        print('Test Finished!')   
-        return episode_returns
+        episode_total_assets = DRLAgent_rllib.DRL_prediction(
+                                  model_name=model_name, 
+                                  env = env,
+                                  price_array=price_array,
+                                  tech_array=tech_array,
+                                  turbulence_array=turbulence_array,
+                                  agent_path = cwd)
 
-          #test using stable baselines3
+        return episode_total_assets
+
+
     elif drl_lib == 'stable_baselines3':
-        from stable_baselines3 import PPO
-
-        try:
-            #load agent
-            model = PPO.load(cwd)
-            print("Successfully load model", cwd)
-        except:
-            raise ValueError('Fail to load agent!')
+        episode_total_assets = DRLAgent_sb3.DRL_prediction_load_from_file(
+                                  model_name=model_name, 
+                                  environment = env_instance,
+                                  cwd = cwd)
         
-        #test on the testing env
-        state = env_instance.reset()
-        episode_returns = list()  # the cumulative_return / initial_account
-        done = False
-        while not done:
-            action = model.predict(state)[0]
-            state, reward, done, _ = env_instance.step(action)
-    
-            total_asset = env_instance.amount + (env_instance.price_ary[env_instance.day] * env_instance.stocks).sum()
-            episode_return = total_asset / env_instance.initial_total_asset
-            episode_returns.append(episode_return)
-        
-        print('episode_return', episode_return)
-        print('Test Finished!')   
-        return episode_returns
-    
+        return episode_total_assets
     else:
-        raise ValueError('DRL library input is NOT supported yet. Please check.')
+        raise ValueError('DRL library input is NOT supported. Please check.')
             
 if __name__ == '__main__':    
-
-    from finrl.neo_finrl.neofinrl_config import FAANG_TICKER
-    from finrl.neo_finrl.neofinrl_config import TECHNICAL_INDICATORS_LIST
-    from finrl.neo_finrl.neofinrl_config import TEST_START_DATE
-    from finrl.neo_finrl.neofinrl_config import TEST_END_DATE
+    from finrl.app.config import DOW_30_TICKER
+    from finrl.app.config import TECHNICAL_INDICATORS_LIST
+    from finrl.app.config import TEST_START_DATE
+    from finrl.app.config import TEST_END_DATE
+    from finrl.app.config import ERL_PARAMS
+    from finrl.app.config import RLlib_PARAMS
+    from finrl.app.config import SAC_PARAMS
 
     #construct environment
     from finrl.neo_finrl.env_stock_trading.env_stock_trading import StockTradingEnv
     env = StockTradingEnv
     
     #demo for elegantrl
-    test(start_date = TEST_START_DATE, end_date = TEST_END_DATE,
-         ticker_list = FAANG_TICKER, data_source = 'yahoofinance',
-         time_interval= '1D', technical_indicator_list= TECHNICAL_INDICATORS_LIST,
-         drl_lib='elegantrl', env=env, agent='ppo', 
-         cwd='./test_ppo', net_dimension = 2 ** 9)
+    account_value_erl=test(start_date = TEST_START_DATE, 
+                           end_date = TEST_END_DATE,
+                           ticker_list = DOW_30_TICKER, 
+                           data_source = 'yahoofinance',
+                           time_interval= '1D', 
+                           technical_indicator_list= TECHNICAL_INDICATORS_LIST,
+                           drl_lib='elegantrl', 
+                           env=env, 
+                           model_name='ppo', 
+                           cwd='./test_ppo', 
+                           net_dimension = 512)
     
     #demo for rllib 
-    test(start_date = TEST_START_DATE, end_date = TEST_END_DATE,
-         ticker_list = FAANG_TICKER, data_source = 'yahoofinance',
-         time_interval= '1D', technical_indicator_list= TECHNICAL_INDICATORS_LIST,
-         drl_lib='rllib', env=env, agent='ppo', 
-         cwd='./test_ppo/checkpoint_000010/checkpoint-10')
+    ray.shutdown() #always shutdown previous session if any
+    account_value_rllib = test(start_date = TEST_START_DATE, 
+                               end_date = TEST_END_DATE,
+                               ticker_list = DOW_30_TICKER, 
+                               data_source = 'yahoofinance',
+                               time_interval= '1D', 
+                               technical_indicator_list= TECHNICAL_INDICATORS_LIST,
+                               drl_lib='rllib', 
+                               env=env, 
+                               model_name='ppo', 
+                               cwd='./test_ppo/checkpoint_000030/checkpoint-30',
+                               rllib_params = RLlib_PARAMS)
 
     #demo for stable baselines3 
-    test(start_date = TEST_START_DATE, end_date = TEST_END_DATE,
-         ticker_list = FAANG_TICKER, data_source = 'yahoofinance',
-         time_interval= '1D', technical_indicator_list= TECHNICAL_INDICATORS_LIST, 
-         drl_lib='stable_baselines3', env=env, agent='ppo', 
-         cwd='./test_ppo.zip')
+    account_value_sb3=test(start_date = TEST_START_DATE, 
+                           end_date = TEST_END_DATE,
+                           ticker_list = DOW_30_TICKER, 
+                           data_source = 'yahoofinance',
+                           time_interval= '1D', 
+                           technical_indicator_list= TECHNICAL_INDICATORS_LIST, 
+                           drl_lib='stable_baselines3', 
+                           env=env, 
+                           model_name='sac', 
+                           cwd='./test_sac.zip')
 
