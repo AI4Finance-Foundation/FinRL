e6bc959d93eac18955cb9f15d25f431f45ae9a42
Bruce Yang <hongyang.yang@columbia.edu>
2021-10-19 22:44:43 +0800

    Update config.py

--- finrl/apps/config.py
+++ finrl/apps/config.py
@@ -34,6 +34,12 @@ RESULTS_DIR = f"results"
 START_DATE = "2009-01-01"
 END_DATE = "2021-01-01"
 
+TRAIN_START_DATE = '2014-01-01'
+TRAIN_END_DATE = '2020-07-30'
+
+TEST_START_DATE = '2020-08-01'
+TEST_END_DATE = '2021-10-01'
+
 START_TRADE_DATE = "2019-01-01"
 
 ## dataset default columns
@@ -41,8 +47,10 @@ DEFAULT_DATA_COLUMNS = ["date", "tic", "close"]
 
 ## stockstats technical indicator column names
 ## check https://pypi.org/project/stockstats/ for different names
-#TECHNICAL_INDICATORS_LIST = ["macd","boll_ub","boll_lb","rsi_30", "cci_30", "dx_30","close_30_sma","close_60_sma"]
-TECHNICAL_INDICATORS_LIST = ["macd","rsi_30", "cci_30", "dx_30"]
+TECHNICAL_INDICATORS_LIST = ["macd","boll_ub","boll_lb","rsi_30", "cci_30", "dx_30","close_30_sma","close_60_sma"]
+#TECHNICAL_INDICATORS_LIST = ["macd","rsi_30", "cci_30", "dx_30"]
+
+
 
 
 ## Model Parameters
