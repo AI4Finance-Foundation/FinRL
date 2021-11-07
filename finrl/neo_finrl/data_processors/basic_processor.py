835bd8c4c4832141757ea2969f5c05fb383de94e
mingzhu <zhumingpassional@gmail.com>
2021-11-07 15:45:43 +0800

    basic class.

--- finrl/neo_finrl/data_processors/basic_processor.py
+++ finrl/neo_finrl/data_processors/basic_processor.py
@@ -1,19 +1,20 @@
 import numpy as np
 import pandas as pd
+from typing import List
 
 class BasicProcessor:
-    def __init__(self, data_source, **kwargs):
+    def __init__(self, data_source: str, **kwargs):
         assert data_source in ["alpaca", "ccxt", "joinquant", "quantconnect", "wrds", "yahoofinance", ], "Data source input is NOT supported yet."
         self.data_source = data_source
 
-    def download_data(self, ticker_list, start_date, end_date, time_interval) -> pd.DataFrame:
+    def download_data(self, ticker_list: List[str], start_date: str, end_date: str, time_interval: str) -> pd.DataFrame:
         pass
 
-    def clean_data(self, df) -> pd.DataFrame:
+    def clean_data(self, df: pd.DataFrame) -> pd.DataFrame:
         pass
 
-    def add_technical_indicator(self, df, tech_indicator_list) -> pd.DataFrame:
+    def add_technical_indicator(self, df: pd.DataFrame, tech_indicator_list: List[str]) -> pd.DataFrame:
         pass
 
-    def add_turbulence(self, df) -> pd.DataFrame:
+    def add_turbulence(self, df: pd.DataFrame) -> pd.DataFrame:
         pass
