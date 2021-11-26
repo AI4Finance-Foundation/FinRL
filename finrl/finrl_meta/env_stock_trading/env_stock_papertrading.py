9e942b8bef664e3b90f3965047ef54eb84d67703
rayrui312 <rayrui@connect.hku.hk>
2021-11-27 00:57:23 +0800

    rename neofinrl to finrl meta

--- finrl/finrl_meta/env_stock_trading/env_stock_papertrading.py
+++ finrl/finrl_meta/env_stock_trading/env_stock_papertrading.py
@@ -1,6 +1,6 @@
 import datetime
 import threading
-from finrl.neo_finrl.data_processors.processor_alpaca import AlpacaProcessor
+from finrl.finrl_meta.data_processors.processor_alpaca import AlpacaProcessor
 import alpaca_trade_api as tradeapi
 import time
 import pandas as pd
