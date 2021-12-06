00cc7fcb821949bf3e885b6e65d01d6b45d8b977
mingzhu <zhumingpassional@gmail.com>
2021-12-06 22:31:27 +0800

    add TIME_ZONE

--- finrl/apps/config.py
+++ finrl/apps/config.py
@@ -81,7 +81,8 @@ TIME_ZONE_USEASTERN = 'US/Eastern'  # Dow, Nasdaq, SP
 TIME_ZONE_PARIS = 'Europe/Paris'  # CAC,
 TIME_ZONE_BERLIN = 'Europe/Berlin'  # DAX, TECDAX, MDAX, SDAX
 TIME_ZONE_JAKARTA = 'Asia/Jakarta'  # LQ45
-TIME_ZONE_SELFDEFINED = 'xxx'  # If neither of the above is your time zone, you should define it.
+TIME_ZONE_SELFDEFINED = 'xxx'  # If neither of the above is your time zone, you should define it, and set USE_TIME_ZONE_SELFDEFINED 1.
+USE_TIME_ZONE_SELFDEFINED = 0  # 0 or 1
 
 ########################################################
 ############## Stock Ticker Setup starts ##############
