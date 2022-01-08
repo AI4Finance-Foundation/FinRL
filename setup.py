95a6b0999f2813e06a632c2fcd219c75ccdb4e26
Bruce Yang <hongyang.yang@columbia.edu>
2022-01-08 21:59:40 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,12 +23,12 @@ setup(
     url="https://github.com/finrl/finrl-library",
     license="MIT",
     packages=find_packages(),
-    #install_requires=REQUIRES
-    #+ ["pyfolio @ git+https://github.com/quantopian/pyfolio.git#egg=pyfolio-0.9.2"]
-    #+ [
-    #    "elegantrl @ git+https://github.com/AI4Finance-Foundation/ElegantRL.git#egg=elegantrl"
-    #],
-    install_requires=REQUIRES,
+    install_requires=REQUIRES
+    + ["pyfolio @ git+https://github.com/quantopian/pyfolio.git#egg=pyfolio-0.9.2"]
+    + [
+        "elegantrl @ git+https://github.com/AI4Finance-Foundation/ElegantRL.git#egg=elegantrl"
+    ],
+    #install_requires=REQUIRES,
     description="FinRL: Financial Reinforcement Learning Framework.",
     #long_description="FinRL is the first open-source framework to demonstrate the great potential of  \
     #applying deep reinforcement learning in quantitative finance. It is developed by `AI4Finance`_. \
