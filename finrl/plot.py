78c3aecbba77e124db17b7426df17a402b88715f
Markus <mysecondworld@firesurf.de>
2022-01-24 23:59:45 +0100

    Inline variable that is immediately returned

--- finrl/plot.py
+++ finrl/plot.py
@@ -25,8 +25,7 @@ def convert_daily_return_to_pyfolio_ts(df):
     strategy_ret.set_index("date", drop=False, inplace=True)
     strategy_ret.index = strategy_ret.index.tz_localize("UTC")
     del strategy_ret["date"]
-    ts = pd.Series(strategy_ret["daily_return"].values, index=strategy_ret.index)
-    return ts
+    return pd.Series(strategy_ret["daily_return"].values, index=strategy_ret.index)
 
 
 def backtest_stats(account_value, value_col_name="account_value"):
@@ -69,10 +68,9 @@ def backtest_plot(
 
 
 def get_baseline(ticker, start, end):
-    dji = YahooDownloader(
+    return YahooDownloader(
         start_date=start, end_date=end, ticker_list=[ticker]
     ).fetch_data()
-    return dji
 
 
 def trx_plot(df_trade, df_actions, ticker_list):
