66e4c932530178f8d3a9a48bfee26c52a7369ae2
MehdiJafari <mehdi_j94@outlook.com>
2022-02-22 11:10:57 +0330

    fixing divided by zero error in .std() in get_validation_sharpe

--- finrl/drl_agents/stablebaselines3/models.py
+++ finrl/drl_agents/stablebaselines3/models.py
@@ -211,15 +211,18 @@ class DRLEnsembleAgent:
         df_total_value = pd.read_csv(
             f"results/account_value_validation_{model_name}_{iteration}.csv"
         )
-        try:
+        # If the agent did not make any transaction 
+        if df_total_value["daily_return"].var()==0:
+            if df_total_value["daily_return"].mean()>0:
+                return (np.inf)
+            else:
+                return (0.0)
+        else:
             return (
-                (4 ** 0.5)
-                * df_total_value["daily_return"].mean()
-                / df_total_value["daily_return"].std()
+                    (4 ** 0.5)
+                    * df_total_value["daily_return"].mean()
+                    / df_total_value["daily_return"].std()
             )
-        except:
-            return (0.0)
-
     def __init__(
             self,
             df,
