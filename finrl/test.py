980bd2526151c9f2ba4bf2cf4a63527eb5819650
Ming Zhu <zhumingpassional@gmail.com>
2022-03-12 18:04:13 +0800

    revise arch

--- finrl/test.py
+++ finrl/test.py
@@ -36,14 +36,14 @@ def test(
     from finrl.finrl_meta.data_processor import DataProcessor
 
     # fetch data
-    DP = DataProcessor(data_source, **kwargs)
-    data = DP.download_data(ticker_list, start_date, end_date, time_interval)
-    data = DP.clean_data(data)
-    data = DP.add_technical_indicator(data, technical_indicator_list)
+    dp = DataProcessor(data_source, **kwargs)
+    data = dp.download_data(ticker_list, start_date, end_date, time_interval)
+    data = dp.clean_data(data)
+    data = dp.add_technical_indicator(data, technical_indicator_list)
 
     if if_vix:
-        data = DP.add_vix(data)
-    price_array, tech_array, turbulence_array = DP.df_to_array(data, if_vix)
+        data = dp.add_vix(data)
+    price_array, tech_array, turbulence_array = dp.df_to_array(data, if_vix)
 
     env_config = {
         "price_array": price_array,
@@ -109,32 +109,34 @@ if __name__ == "__main__":
         net_dimension=512,
     )
 
-    # demo for rllib
-    ray.shutdown()  # always shutdown previous session if any
-    account_value_rllib = test(
-        start_date=TEST_START_DATE,
-        end_date=TEST_END_DATE,
-        ticker_list=DOW_30_TICKER,
-        data_source="yahoofinance",
-        time_interval="1D",
-        technical_indicator_list=TECHNICAL_INDICATORS_LIST,
-        drl_lib="rllib",
-        env=env,
-        model_name="ppo",
-        cwd="./test_ppo/checkpoint_000030/checkpoint-30",
-        rllib_params=RLlib_PARAMS,
-    )
+    ## if users want to use rllib, or stable-baselines3, users can remove the following comments
 
-    # demo for stable baselines3
-    account_value_sb3 = test(
-        start_date=TEST_START_DATE,
-        end_date=TEST_END_DATE,
-        ticker_list=DOW_30_TICKER,
-        data_source="yahoofinance",
-        time_interval="1D",
-        technical_indicator_list=TECHNICAL_INDICATORS_LIST,
-        drl_lib="stable_baselines3",
-        env=env,
-        model_name="sac",
-        cwd="./test_sac.zip",
-    )
+    # # demo for rllib
+    # ray.shutdown()  # always shutdown previous session if any
+    # account_value_rllib = test(
+    #     start_date=TEST_START_DATE,
+    #     end_date=TEST_END_DATE,
+    #     ticker_list=DOW_30_TICKER,
+    #     data_source="yahoofinance",
+    #     time_interval="1D",
+    #     technical_indicator_list=TECHNICAL_INDICATORS_LIST,
+    #     drl_lib="rllib",
+    #     env=env,
+    #     model_name="ppo",
+    #     cwd="./test_ppo/checkpoint_000030/checkpoint-30",
+    #     rllib_params=RLlib_PARAMS,
+    # )
+    #
+    # # demo for stable baselines3
+    # account_value_sb3 = test(
+    #     start_date=TEST_START_DATE,
+    #     end_date=TEST_END_DATE,
+    #     ticker_list=DOW_30_TICKER,
+    #     data_source="yahoofinance",
+    #     time_interval="1D",
+    #     technical_indicator_list=TECHNICAL_INDICATORS_LIST,
+    #     drl_lib="stable_baselines3",
+    #     env=env,
+    #     model_name="sac",
+    #     cwd="./test_sac.zip",
+    # )
