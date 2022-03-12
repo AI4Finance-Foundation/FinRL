7258d6f0ba1cbfd0ab1670327ca35a7264cda193
Ming Zhu <zhumingpassional@gmail.com>
2022-03-12 17:55:46 +0800

    revise arch

--- finrl/config.py
+++ finrl/config.py
@@ -1,22 +1,10 @@
-# pd.options.display.max_rows = 10
-# pd.options.display.max_columns = 10
-# PACKAGE_ROOT = pathlib.Path(finrl.__file__).resolve().parent
-# PACKAGE_ROOT = pathlib.Path().resolve().parent
-
-# DATASET_DIR = PACKAGE_ROOT / "data"
-
-# data
-# TRAINING_DATA_FILE = "data/ETF_SPY_2009_2020.csv"
-# TURBULENCE_DATA = "data/dow30_turbulence_index.csv"
-# TESTING_DATA_FILE = "test.csv"
-
-# now = datetime.datetime.now()
+# dir
 DATA_SAVE_DIR = "datasets"
 TRAINED_MODEL_DIR = "trained_models"
 TENSORBOARD_LOG_DIR = "tensorboard_log"
 RESULTS_DIR = "results"
 
-## time_fmt = '%Y-%m-%d'
+# time format: '%Y-%m-%d'
 START_DATE = "2009-01-01"
 END_DATE = "2021-10-31"
 
@@ -28,11 +16,11 @@ TEST_END_DATE = "2021-10-01"
 
 TRADE_START_DATE = "2021-11-01"
 
-## dataset default columns
+# dataset default columns
 DEFAULT_DATA_COLUMNS = ["date", "tic", "close"]
 
-## stockstats technical indicator column names
-## check https://pypi.org/project/stockstats/ for different names
+# stockstats technical indicator column names
+# check https://pypi.org/project/stockstats/ for different names
 TECHNICAL_INDICATORS_LIST = [
     "macd",
     "boll_ub",
@@ -46,7 +34,7 @@ TECHNICAL_INDICATORS_LIST = [
 # TECHNICAL_INDICATORS_LIST = ["macd","rsi_30", "cci_30", "dx_30"]
 
 
-## Model Parameters
+# Model Parameters
 A2C_PARAMS = {"n_steps": 5, "ent_coef": 0.01, "learning_rate": 0.0007}
 PPO_PARAMS = {
     "n_steps": 2048,
@@ -74,8 +62,8 @@ ERL_PARAMS = {
 }
 RLlib_PARAMS = {"lr": 5e-5, "train_batch_size": 500, "gamma": 0.99}
 
-########################################################
-#######Possible time zones##############################
+
+# Possible time zones
 TIME_ZONE_SHANGHAI = 'Asia/Shanghai'  ## Hang Seng HSI, SSE, CSI
 TIME_ZONE_USEASTERN = 'US/Eastern'  # Dow, Nasdaq, SP
 TIME_ZONE_PARIS = 'Europe/Paris'  # CAC,
