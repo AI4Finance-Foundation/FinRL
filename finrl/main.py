ff0125f99ce4927becc6aea2bd91bee4665f7d89
Ming Zhu <zhumingpassional@gmail.com>
2022-03-14 23:04:13 +0800

    revise arch

--- finrl/main.py
+++ finrl/main.py
@@ -3,6 +3,20 @@ from argparse import ArgumentParser
 
 from finrl import config
 
+from finrl.config_tickers import DOW_30_TICKER
+from finrl.config import (
+    TECHNICAL_INDICATORS_LIST,
+    TRAIN_START_DATE,
+    TRAIN_END_DATE,
+    ERL_PARAMS,
+    RLlib_PARAMS,
+    SAC_PARAMS,
+)
+
+from finrl.finrl_meta.data_processor import DataProcessor
+
+# construct environment
+from finrl.finrl_meta.env_stock_trading.env_stocktrading_np import StockTradingEnv
 
 def build_parser():
     parser = ArgumentParser()
@@ -29,9 +43,27 @@ def main():
         os.makedirs("./" + config.RESULTS_DIR)
 
     if options.mode == "train":
-        import finrl.train
+        from finrl import train
 
-        finrl.train.train_stock_trading()
+        env = StockTradingEnv
+
+        # demo for elegantrl
+        kwargs = {}  # in current finrl_meta, with respect yahoofinance, kwargs is {}. For other data sources, such as joinquant, kwargs is not empty
+        train(
+            start_date=TRAIN_START_DATE,
+            end_date=TRAIN_END_DATE,
+            ticker_list=DOW_30_TICKER,
+            data_source="yahoofinance",
+            time_interval="1D",
+            technical_indicator_list=TECHNICAL_INDICATORS_LIST,
+            drl_lib="elegantrl",
+            env=env,
+            model_name="ppo",
+            cwd="./test_ppo",
+            erl_params=ERL_PARAMS,
+            break_step=1e5,
+            kwargs=kwargs,
+        )
 
 
 if __name__ == "__main__":
