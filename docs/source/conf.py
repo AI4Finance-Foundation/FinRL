790459654dcaedc3956fee13ea3666fd3a6c96e5
ZiyiXia <77958037+ZiyiXia@users.noreply.github.com>
2022-03-30 07:30:42 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -97,7 +97,11 @@ pygments_style = None
 
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_logo = "../img/logo.jpg"
+html_logo = "./image/logo_transparent_background.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
