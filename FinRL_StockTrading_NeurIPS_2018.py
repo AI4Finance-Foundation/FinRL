a0c5fb41bee29174125b5ee9414c89bd72935d44
Ming Zhu <zhumingpassional@gmail.com>
2022-04-08 22:03:23 +0800

    add python version

--- FinRL_StockTrading_NeurIPS_2018.py
+++ FinRL_StockTrading_NeurIPS_2018.py
@@ -44,47 +44,48 @@
 # # Part 1. Problem Definition
 #
 # #%% md
-#
-# This problem is to design an automated trading solution for single stock trading. We model the stock trading process as a Markov Decision Process (MDP). We then formulate our trading goal as a maximization problem.
-#
-# The algorithm is trained using Deep Reinforcement Learning (DRL) algorithms and the components of the reinforcement learning environment are:
-#
-#
-# * Action: The action space describes the allowed actions that the agent interacts with the
-# environment. Normally, a ∈ A includes three actions: a ∈ {−1, 0, 1}, where −1, 0, 1 represent
-# selling, holding, and buying one stock. Also, an action can be carried upon multiple shares. We use
-# an action space {−k, ..., −1, 0, 1, ..., k}, where k denotes the number of shares. For example, "Buy
-# 10 shares of AAPL" or "Sell 10 shares of AAPL" are 10 or −10, respectively
-#
-# * Reward function: r(s, a, s′) is the incentive mechanism for an agent to learn a better action. The change of the portfolio value when action a is taken at state s and arriving at new state s',  i.e., r(s, a, s′) = v′ − v, where v′ and v represent the portfolio
-# values at state s′ and s, respectively
-#
-# * State: The state space describes the observations that the agent receives from the environment. Just as a human trader needs to analyze various information before executing a trade, so
-# our trading agent observes many different features to better learn in an interactive environment.
-#
-# * Environment: Dow 30 consituents
-#
-#
-# The data of the single stock that we will be using for this case study is obtained from Yahoo Finance API. The data contains Open-High-Low-Close price and volume.
-#
-#
-# #%% md
-#
-# <a id='1'></a>
-# # Part 2. Getting Started- Load Python Packages
-#
-# #%% md
-#
-# <a id='1.1'></a>
-# ## 2.1. Install all the packages through FinRL library
-#
-#
-# #%%
-#
-# ## install finrl library
-# !pip install git+https://github.com/AI4Finance-LLC/FinRL-Library.git
-#
-# #%%
+'''
+This problem is to design an automated trading solution for single stock trading. We model the stock trading process as a Markov Decision Process (MDP). We then formulate our trading goal as a maximization problem.
+
+The algorithm is trained using Deep Reinforcement Learning (DRL) algorithms and the components of the reinforcement learning environment are:
+
+
+* Action: The action space describes the allowed actions that the agent interacts with the
+environment. Normally, a ∈ A includes three actions: a ∈ {−1, 0, 1}, where −1, 0, 1 represent
+selling, holding, and buying one stock. Also, an action can be carried upon multiple shares. We use
+an action space {−k, ..., −1, 0, 1, ..., k}, where k denotes the number of shares. For example, "Buy
+10 shares of AAPL" or "Sell 10 shares of AAPL" are 10 or −10, respectively
+
+* Reward function: r(s, a, s′) is the incentive mechanism for an agent to learn a better action. The change of the portfolio value when action a is taken at state s and arriving at new state s',  i.e., r(s, a, s′) = v′ − v, where v′ and v represent the portfolio
+values at state s′ and s, respectively
+
+* State: The state space describes the observations that the agent receives from the environment. Just as a human trader needs to analyze various information before executing a trade, so
+our trading agent observes many different features to better learn in an interactive environment.
+
+* Environment: Dow 30 consituents
+
+
+The data of the single stock that we will be using for this case study is obtained from Yahoo Finance API. The data contains Open-High-Low-Close price and volume.
+
+
+#%% md
+
+<a id='1'></a>
+# Part 2. Getting Started- Load Python Packages
+
+#%% md
+
+<a id='1.1'></a>
+## 2.1. Install all the packages through FinRL library
+
+
+#%%
+
+## install finrl library
+!pip install git+https://github.com/AI4Finance-LLC/FinRL-Library.git
+
+#%%
+'''
 
 from finrl import config
 from finrl import config_tickers
@@ -101,24 +102,8 @@ if not os.path.exists("./" + config.RESULTS_DIR):
 #%% md
 
 
-# <a id='1.2'></a>
-# ## 2.2. Check if the additional packages needed are present, if not install them.
-# * Yahoo Finance API
-# * pandas
-# * numpy
-# * matplotlib
-# * stockstats
-# * OpenAI gym
-# * stable-baselines
-# * tensorflow
-# * pyfolio
-#
-# #%% md
-#
-# <a id='1.3'></a>
+
 # ## 2.3. Import Packages
-#
-# #%%
 
 import pandas as pd
 import numpy as np
@@ -142,55 +127,56 @@ sys.path.append("../FinRL-Library")
 
 import itertools
 
+'''
+%% md
+
+<a id='1.4'></a>
+## 2.4. Create Folders
+
 #%% md
-#
-# <a id='1.4'></a>
-# ## 2.4. Create Folders
-#
-# #%% md
-#
-# <a id='2'></a>
-# # Part 3. Download Data
-# Yahoo Finance is a website that provides stock data, financial news, financial reports, etc. All the data provided by Yahoo Finance is free.
-# * FinRL uses a class **YahooDownloader** to fetch data from Yahoo Finance API
-# * Call Limit: Using the Public API (without authentication), you are limited to 2,000 requests per hour per IP (or up to a total of 48,000 requests a day).
-#
-#
-# #%% md
+
+<a id='2'></a>
+# Part 3. Download Data
+Yahoo Finance is a website that provides stock data, financial news, financial reports, etc. All the data provided by Yahoo Finance is free.
+* FinRL uses a class **YahooDownloader** to fetch data from Yahoo Finance API
+* Call Limit: Using the Public API (without authentication), you are limited to 2,000 requests per hour per IP (or up to a total of 48,000 requests a day).
 
 
+#%% md
 
-# # -----
-# class YahooDownloader:
-    # Provides methods for retrieving daily stock data from
-    # Yahoo Finance API
-    #
-    # Attributes
-    # ----------
-    #     start_date : str
-    #         start date of the data (modified from config.py)
-    #     end_date : str
-    #         end date of the data (modified from config.py)
-    #     ticker_list : list
-    #         a list of stock tickers (modified from config.py)
-    #
-    # Methods
-    # -------
-    # fetch_data()
-    #     Fetches data from yahoo API
 
+# -----
+class YahooDownloader:
+    Provides methods for retrieving daily stock data from
+    Yahoo Finance API
+
+    Attributes
+    ----------
+        start_date : str
+            start date of the data (modified from config.py)
+        end_date : str
+            end date of the data (modified from config.py)
+        ticker_list : list
+            a list of stock tickers (modified from config.py)
+
+    Methods
+    -------
+    fetch_data()
+        Fetches data from yahoo API
+
+
+%%
+
+# from config.py TRAIN_START_DATE is a string
+config.TRAIN_START_DATE
 
 #%%
-#
-# # from config.py TRAIN_START_DATE is a string
-# config.TRAIN_START_DATE
-#
-# #%%
-#
-# # from config.py TRAIN_END_DATE is a string
-# config.TRAIN_END_DATE
-#
-# #%%
+
+# from config.py TRAIN_END_DATE is a string
+config.TRAIN_END_DATE
+
+#%%
+'''
 
 df = YahooDownloader(start_date = '2009-01-01',
                      end_date = '2021-10-31',
@@ -210,12 +196,14 @@ df.sort_values(['date','tic'],ignore_index=True).head()
 
 #%% md
 
-# # Part 4: Preprocess Data
-# Data preprocessing is a crucial step for training a high quality machine learning model. We need to check for missing data and do feature engineering in order to convert the data into a model-ready state.
-# * Add technical indicators. In practical trading, various information needs to be taken into account, for example the historical stock prices, current holding shares, technical indicators, etc. In this article, we demonstrate two trend-following technical indicators: MACD and RSI.
-# * Add turbulence index. Risk-aversion reflects whether an investor will choose to preserve the capital. It also influences one's trading strategy when facing different market volatility level. To control the risk in a worst-case scenario, such as financial crisis of 2007–2008, FinRL employs the financial turbulence index that measures extreme asset price fluctuation.
-#
-# #%%
+'''
+# Part 4: Preprocess Data
+Data preprocessing is a crucial step for training a high quality machine learning model. We need to check for missing data and do feature engineering in order to convert the data into a model-ready state.
+* Add technical indicators. In practical trading, various information needs to be taken into account, for example the historical stock prices, current holding shares, technical indicators, etc. In this article, we demonstrate two trend-following technical indicators: MACD and RSI.
+* Add turbulence index. Risk-aversion reflects whether an investor will choose to preserve the capital. It also influences one's trading strategy when facing different market volatility level. To control the risk in a worst-case scenario, such as financial crisis of 2007–2008, FinRL employs the financial turbulence index that measures extreme asset price fluctuation.
+
+#%%
+'''
 
 fe = FeatureEngineer(
                     use_technical_indicator=True,
@@ -242,22 +230,20 @@ processed_full = processed_full.fillna(0)
 
 processed_full.sort_values(['date','tic'],ignore_index=True).head(10)
 
-# #%% md
-#
-# <a id='4'></a>
-# # Part 5. Design Environment
-# Considering the stochastic and interactive nature of the automated stock trading tasks, a financial task is modeled as a **Markov Decision Process (MDP)** problem. The training process involves observing stock price change, taking an action and reward's calculation to have the agent adjusting its strategy accordingly. By interacting with the environment, the trading agent will derive a trading strategy with the maximized rewards as time proceeds.
-#
-# Our trading environments, based on OpenAI Gym framework, simulate live stock markets with real market data according to the principle of time-driven simulation.
-#
-# The action space describes the allowed actions that the agent interacts with the environment. Normally, action a includes three actions: {-1, 0, 1}, where -1, 0, 1 represent selling, holding, and buying one share. Also, an action can be carried upon multiple shares. We use an action space {-k,…,-1, 0, 1, …, k}, where k denotes the number of shares to buy and -k denotes the number of shares to sell. For example, "Buy 10 shares of AAPL" or "Sell 10 shares of AAPL" are 10 or -10, respectively. The continuous action space needs to be normalized to [-1, 1], since the policy is defined on a Gaussian distribution, which needs to be normalized and symmetric.
-#
-#%% md
+'''
+# Part 5. Design Environment
+Considering the stochastic and interactive nature of the automated stock trading tasks, a financial task is modeled as a **Markov Decision Process (MDP)** problem. The training process involves observing stock price change, taking an action and reward's calculation to have the agent adjusting its strategy accordingly. By interacting with the environment, the trading agent will derive a trading strategy with the maximized rewards as time proceeds.
 
-## Training data split: 2009-01-01 to 2020-07-01
-## Trade data split: 2020-07-01 to 2021-10-31
+Our trading environments, based on OpenAI Gym framework, simulate live stock markets with real market data according to the principle of time-driven simulation.
 
-#%%
+The action space describes the allowed actions that the agent interacts with the environment. Normally, action a includes three actions: {-1, 0, 1}, where -1, 0, 1 represent selling, holding, and buying one share. Also, an action can be carried upon multiple shares. We use an action space {-k,…,-1, 0, 1, …, k}, where k denotes the number of shares to buy and -k denotes the number of shares to sell. For example, "Buy 10 shares of AAPL" or "Sell 10 shares of AAPL" are 10 or -10, respectively. The continuous action space needs to be normalized to [-1, 1], since the policy is defined on a Gaussian distribution, which needs to be normalized and symmetric.
+
+%% md
+
+# Training data split: 2009-01-01 to 2020-07-01
+# Trade data split: 2020-07-01 to 2021-10-31
+'''
+
 
 train = data_split(processed_full, '2009-01-01','2020-07-01')
 trade = data_split(processed_full, '2020-07-01','2021-10-31')
@@ -317,21 +303,22 @@ print(f"type(env_train): {type(env_train)}")
 
 #%% md
 
-# <a id='5'></a>
-# # Part 6: Implement DRL Algorithms
-# * The implementation of the DRL algorithms are based on **OpenAI Baselines** and **Stable Baselines**. Stable Baselines is a fork of OpenAI Baselines, with a major structural refactoring, and code cleanups.
-# * FinRL library includes fine-tuned standard DRL algorithms, such as DQN, DDPG,
-# Multi-Agent DDPG, PPO, SAC, A2C and TD3. We also allow users to
-# design their own DRL algorithms by adapting these DRL algorithms.
+'''
+# Part 6: Implement DRL Algorithms
+* The implementation of the DRL algorithms are based on **OpenAI Baselines** and **Stable Baselines**. Stable Baselines is a fork of OpenAI Baselines, with a major structural refactoring, and code cleanups.
+* FinRL library includes fine-tuned standard DRL algorithms, such as DQN, DDPG,
+Multi-Agent DDPG, PPO, SAC, A2C and TD3. We also allow users to
+design their own DRL algorithms by adapting these DRL algorithms.
+'''
 
-#%%
 
 agent = DRLAgent(env = env_train)
 
 #%% md
 
-### Model Training: 5 models, A2C DDPG, PPO, TD3, SAC
-
+'''
+Model Training: 5 models, A2C DDPG, PPO, TD3, SAC
+'''
 
 #%% md
 
@@ -429,15 +416,16 @@ trained_sac = agent.train_model(model=model_sac,
 
 #%% md
 
-# ## Trading
-# Assume that we have $1,000,000 initial capital at 2020-07-01. We use the DDPG model to trade Dow jones 30 stocks.
-#
-# #%% md
-#
-# ### Set turbulence threshold
-# Set the turbulence threshold to be greater than the maximum of insample turbulence data, if current turbulence index is greater than the threshold, then we assume that the current market is volatile
+'''
+## Trading
+Assume that we have $1,000,000 initial capital at 2020-07-01. We use the DDPG model to trade Dow jones 30 stocks.
 
-#%%
+#%% md
+
+### Set turbulence threshold
+Set the turbulence threshold to be greater than the maximum of insample turbulence data, if current turbulence index is greater than the threshold, then we assume that the current market is volatile
+'''
+
 
 data_risk_indicator = processed_full[(processed_full.date<'2020-07-01') & (processed_full.date>='2009-01-01')]
 insample_risk_indicator = data_risk_indicator.drop_duplicates(subset=['date'])
@@ -460,6 +448,7 @@ insample_risk_indicator.turbulence.quantile(0.996)
 
 #%% md
 
+'''
 # ### Trade
 #
 # DRL model needs to update periodically in order to take full advantage of the data, ideally we need to retrain our model yearly, quarterly, or monthly. We also need to tune the parameters along the way, in this notebook I only use the in-sample data from 2009-01 to 2020-07 to tune the parameters once, so there is some alpha decay here as the length of trade date extends.
@@ -467,6 +456,7 @@ insample_risk_indicator.turbulence.quantile(0.996)
 # Numerous hyperparameters – e.g. the learning rate, the total number of samples to train on – influence the learning process and are usually determined by testing some variations.
 #
 # #%%
+'''
 
 #trade = data_split(processed_full, '2020-07-01','2021-10-31')
 e_trade_gym = StockTradingEnv(df = trade, turbulence_threshold = 70,risk_indicator_col='vix', **env_kwargs)
@@ -496,16 +486,18 @@ print(f"df_actions.head(): {df_actions.head()}")
 
 #%% md
 
-# <a id='6'></a>
-# # Part 7: Backtest Our Strategy
-# Backtesting plays a key role in evaluating the performance of a trading strategy. Automated backtesting tool is preferred because it reduces the human error. We usually use the Quantopian pyfolio package to backtest our trading strategies. It is easy to use and consists of various individual plots that provide a comprehensive image of the performance of a trading strategy.
-#
-# #%% md
-#
-# <a id='6.1'></a>
-## 7.1 BackTestStats
-# pass in df_account_value, this information is stored in env class
 
+'''
+# # Part 7: Backtest Our Strategy
+Backtesting plays a key role in evaluating the performance of a trading strategy. Automated backtesting tool is preferred because it reduces the human error. We usually use the Quantopian pyfolio package to backtest our trading strategies. It is easy to use and consists of various individual plots that provide a comprehensive image of the performance of a trading strategy.
+
+#%% md
+'''
+
+'''
+# 7.1 BackTestStats
+pass in df_account_value, this information is stored in env class
+'''
 
 #%%
 
@@ -553,10 +545,5 @@ backtest_plot(df_account_value,
              baseline_start = df_account_value.loc[0,'date'],
              baseline_end = df_account_value.loc[len(df_account_value)-1,'date'])
 
-#%%
-
-
-
-#%%
 
 
