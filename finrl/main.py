1dbedf7dcb34e16f3f331f47bbd9ab73befa2e05
Ming Zhu <zhumingpassional@gmail.com>
2022-04-09 16:47:58 +0800

    add make dir

--- finrl/main.py
+++ finrl/main.py
@@ -46,16 +46,6 @@ def check_and_make_directories(directories: List[str]):
             os.makedirs("./" + directory)
 
 
-def check_make_directories():
-    if not os.path.exists("./" + config.DATA_SAVE_DIR):
-        os.makedirs("./" + config.DATA_SAVE_DIR)
-    if not os.path.exists("./" + config.TRAINED_MODEL_DIR):
-        os.makedirs("./" + config.TRAINED_MODEL_DIR)
-    if not os.path.exists("./" + config.TENSORBOARD_LOG_DIR):
-        os.makedirs("./" + config.TENSORBOARD_LOG_DIR)
-    if not os.path.exists("./" + config.RESULTS_DIR):
-        os.makedirs("./" + config.RESULTS_DIR)
-
 
 def main():
     parser = build_parser()
