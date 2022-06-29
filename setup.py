e53ae8c9ddac04c78253f1590a9e8cb5c657800a
Dominique Garmier <dominique@garmier.ch>
2022-06-29 22:35:03 +0200

    bumped version support to python3.8 since https://pypi.org/project/exchange-calendars/ only supports down to 3.8

--- setup.py
+++ setup.py
@@ -24,10 +24,10 @@ setup(
     license="MIT",
     packages=find_packages(),
     install_requires=REQUIRES
-                     + ["pyfolio @ git+https://github.com/quantopian/pyfolio.git#egg=pyfolio-0.9.2"]
-                     + [
-                         "elegantrl @ git+https://github.com/AI4Finance-Foundation/ElegantRL.git#egg=elegantrl"
-                     ],
+    + ["pyfolio @ git+https://github.com/quantopian/pyfolio.git#egg=pyfolio-0.9.2"]
+    + [
+        "elegantrl @ git+https://github.com/AI4Finance-Foundation/ElegantRL.git#egg=elegantrl"
+    ],
     # install_requires=REQUIRES,
     description="FinRL: Financial Reinforcement Learning Framework.",
     # long_description="FinRL is the first open-source framework to demonstrate the great potential of  \
