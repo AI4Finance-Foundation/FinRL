17252be8bc38031aa540e78f64b08fab39fea58a
dev <dev@gmail.com>
2022-08-08 12:04:13 +0200

    Add day bar test

--- tests/downloaders/test_alpaca_downloader.py
+++ tests/downloaders/test_alpaca_downloader.py
@@ -16,7 +16,7 @@ def ticker_list():
     return ["AAPL", "GOOG"]
 
 
-def test_intraDay_download(ticker_list):
+def test_intraDayBar_download(ticker_list):
     # Given
     start_date = '2021-07-29'
     end_date = '2021-07-30'
@@ -42,3 +42,34 @@ def test_intraDay_download(ticker_list):
 
     assert data.shape == expected_shape
     assert (data[data['tic'] == 'GOOG'].head(1) == expected_head_1).all(axis=None)
+
+def test_dayBar_download(ticker_list):
+    # Given
+    start_date = '2021-07-29'
+    end_date = '2021-07-30'
+    time_interval = '1D'
+    ticker_list = ["AAPL", "GOOG"]
+
+    # When
+    DP = AlpacaProcessor(
+                      API_KEY = API_KEY,
+                      API_SECRET = API_SECRET,
+                      API_BASE_URL = API_BASE_URL
+                      )
+    data = DP.download_data(start_date = start_date,
+                        end_date = end_date,
+                        ticker_list = ticker_list,
+                        time_interval= time_interval)
+
+    # Then
+    expected = pd.DataFrame([['2021-07-29 04:00:00', 144.66, 146.55, 144.58, 145.64, 56571097,
+        414821, 145.806207, 'AAPL'],
+       ['2021-07-30 04:00:00', 144.49, 146.33, 144.11, 145.86, 70291908,
+        464977, 145.396798, 'AAPL'],
+       ['2021-07-29 04:00:00', 2722.76, 2743.03, 2722.76, 2730.81,
+        962833, 56703, 2732.943535, 'GOOG'],
+       ['2021-07-30 04:00:00', 2710.22, 2715.4273, 2696.284, 2704.42,
+        1192648, 55739, 2704.685882, 'GOOG']],columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'trade_count',
+       'vwap', 'tic'])
+
+    assert data == expected
