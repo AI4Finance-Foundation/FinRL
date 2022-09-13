b87161236870d8aa2f25c2efcdbced1550a1565d
quantumiracle <zhding@mail.ustc.edu.cn>
2022-09-13 15:54:18 -0400

    fix download data for train/test

--- tutorials/3-Practical/train_alpaca.py
+++ tutorials/3-Practical/train_alpaca.py
@@ -3,9 +3,6 @@ from finrl.test import test
 from finrl.config_tickers import DOW_30_TICKER
 from finrl.config import INDICATORS
 from finrl.meta.env_stock_trading.env_stocktrading_np import StockTradingEnv
-from finrl.meta.env_stock_trading.env_stock_papertrading import AlpacaPaperTrading
-from finrl.meta.data_processor import DataProcessor
-from finrl.plot import backtest_stats, backtest_plot, get_daily_return, get_baseline
 from common import *
 
 import numpy as np
