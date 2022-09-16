f35eb61f647533d92adc6de44d404caa15600493
zihan <zhding@mail.ustc.edu.cn>
2022-09-15 22:06:42 -0400

    backtest

--- tutorials/3-Practical/backtest_alpaca.py
+++ tutorials/3-Practical/backtest_alpaca.py
@@ -14,8 +14,8 @@ candle_time_interval = '1Min'  # '1Min'
 
 env = StockTradingEnv
 
-start_date = '2022-3-1'
-end_date = '2022-6-1'
+start_date = '2019-1-1'
+end_date = '2022-1-1'
 
 account_value = test(start_date = start_date, 
       end_date = end_date,
