c4b1f1705bff9d827cbc7fc4901805ff4034ab25
Cristian Arley Quintero Gonzalez <7codev@gmail.com>
2022-10-12 23:19:16 +0200

    modification on file Stock_NeurIPS2018_SB3 with issues because of docstring checking

--- Stock_NeurIPS2018_SB3.py
+++ Stock_NeurIPS2018_SB3.py
@@ -105,14 +105,14 @@ from finrl.config import (
 """
 Use check_and_make_directories() to replace the following
 
-if not os.path.exists("./" + config.DATA_SAVE_DIR):
-    os.makedirs("./" + config.DATA_SAVE_DIR)
-if not os.path.exists("./" + config.TRAINED_MODEL_DIR):
-    os.makedirs("./" + config.TRAINED_MODEL_DIR)
-if not os.path.exists("./" + config.TENSORBOARD_LOG_DIR):
-    os.makedirs("./" + config.TENSORBOARD_LOG_DIR)
-if not os.path.exists("./" + config.RESULTS_DIR):
-    os.makedirs("./" + config.RESULTS_DIR)
+if not os.path.exists('/' + config.DATA_SAVE_DIR):
+    os.makedirs('./' + config.DATA_SAVE_DIR)
+if not os.path.exists('./' + config.TRAINED_MODEL_DIR):
+    os.makedirs('./' + config.TRAINED_MODEL_DIR)
+if not os.path.exists('./' + config.TENSORBOARD_LOG_DIR):
+    os.makedirs('./' + config.TENSORBOARD_LOG_DIR)
+if not os.path.exists('./' + config.RESULTS_DIR):
+    os.makedirs('./' + config.RESULTS_DIR)
 
 """
 
