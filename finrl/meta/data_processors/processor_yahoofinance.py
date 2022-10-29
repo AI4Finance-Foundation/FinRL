bd0aed57e21e3bf0333f3bbe23b36bdacfb6b133
marc <marc@hollyoak.org.uk>
2022-10-29 20:04:56 +0100

    U

--- finrl/meta/data_processors/processor_yahoofinance.py
+++ finrl/meta/data_processors/processor_yahoofinance.py
@@ -93,6 +93,8 @@ class YahooFinanceProcessor:
         print("end_date: ", end_date)
 
         # Download and save the data in a pandas DataFrame:
+        start_date = date(start_date)
+        end_date = date(end_date)
         delta = timedelta(days=1)
         data_df = pd.DataFrame()
         for tic in ticker_list:
