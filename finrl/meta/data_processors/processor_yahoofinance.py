d45219bbaf7470fe25b6707758aa38e492773762
marc <marc@hollyoak.org.uk>
2022-10-31 20:36:03 +0000

    U

--- finrl/meta/data_processors/processor_yahoofinance.py
+++ finrl/meta/data_processors/processor_yahoofinance.py
@@ -183,7 +183,7 @@ class YahooFinanceProcessor:
                         previous_close,
                         0.0,
                     ]
-                    #print(tmp_df.iloc[i]["index"], " Filled NaN data with previous close and set volume to 0. ticker: ", tic)
+                    print(tmp_df.iloc[i], " Filled NaN data with previous close and set volume to 0. ticker: ", tic)
 
             print("after filling nan data - tmp_df:\n", tmp_df)
             # merge single ticker data to new DataFrame
