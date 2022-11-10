2222d93047fdf5ec5566a4695981c40f8ef9c8df
zihan <zhding@mail.ustc.edu.cn>
2022-11-10 09:09:42 -0500

    fix

--- tutorials/3-Practical/common.py
+++ tutorials/3-Practical/common.py
@@ -1,4 +1,4 @@
-API_KEY = "PKBY8Z18D1DVI67T7PCP"
+API_KEY = ""
 API_SECRET = "twKNqcDqgdubM6Y0hrSiFkmOwuqP2oUTA5zxkfdB"
 API_BASE_URL = 'https://paper-api.alpaca.markets'
 data_url = 'wss://data.alpaca.markets'
