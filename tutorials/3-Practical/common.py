4827d20f05ef9a69aef93d24ed1b95f799cedcb0
quantumiracle <zhding@mail.ustc.edu.cn>
2022-11-10 09:10:37 -0500

    update key

--- tutorials/3-Practical/common.py
+++ tutorials/3-Practical/common.py
@@ -1,4 +1,4 @@
-API_KEY = ""
-API_SECRET = "twKNqcDqgdubM6Y0hrSiFkmOwuqP2oUTA5zxkfdB"
+API_KEY = "PKRG5QHY0R3L7J1X0XDD"
+API_SECRET = "0MvvQuWhm3Pblu81h7IZxbb8y1roGQprSTyh6Gh7"
 API_BASE_URL = 'https://paper-api.alpaca.markets'
 data_url = 'wss://data.alpaca.markets'
