62426f123564ed522a983dc8c33366665d1e77a1
marc <marc@hollyoak.org.uk>
2022-12-12 19:48:25 +0000

    U

--- finrl/meta/env_stock_trading/env_stock_papertrading.py
+++ finrl/meta/env_stock_trading/env_stock_papertrading.py
@@ -16,7 +16,7 @@ from finrl.meta.data_processors.processor_alpaca import AlpacaProcessor
 class AlpacaPaperTrading:
     def __init__(
         self,
-        tickeStockEnvEmptyr_list,
+        ticker_list,
         time_interval,
         drl_lib,
         agent,
