2f834d96eaaf0358aae99b8a2700852fe50e56a3
quantumiracle <zhding@mail.ustc.edu.cn>
2023-02-11 22:27:19 -0500

    multiprocess for streamlit train

--- tutorials/3-Practical/streamlit_train.py
+++ tutorials/3-Practical/streamlit_train.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import datetime  as dt
+import multiprocessing
 from finrl.train import train
 from finrl.test import test
 from finrl.config_tickers import *
@@ -29,6 +30,27 @@ ticker_list = eval(StockPool)[:int(NumStock)]
 env = StockTradingEnv
 save_path = f'./papertrading_erl/{MODEL_IDX}/'
 
+def train_process(**kwargs):
+#     for key, value in kwargs.items():     
+      train(start_date=kwargs['start_date'],
+            end_date=kwargs['end_date'],
+            ticker_list=kwargs['ticker_list'],
+            data_source=kwargs['data_source'],
+            time_interval=kwargs['time_interval'],
+            technical_indicator_list=kwargs['technical_indicator_list'],
+            drl_lib=kwargs['drl_lib'],
+            env=kwargs['env'],
+            model_name=kwargs['model_name'],
+            API_KEY=kwargs['API_KEY'],
+            API_SECRET=kwargs['API_SECRET'],
+            API_BASE_URL=kwargs['API_BASE_URL'],
+            erl_params=kwargs['erl_params'],
+            cwd=kwargs['cwd'],  # current_working_dir
+            wandb=kwargs['wandb'],
+            break_step=kwargs['break_step']
+            )
+
+
 if st.button('Train'):
       ERL_PARAMS = {"learning_rate": 3e-6, "batch_size": 2048, "gamma": 0.985,
               "seed": 312, "net_dimension": 512, "target_step": 5000, "eval_gap": 30,
@@ -36,23 +58,46 @@ if st.button('Train'):
       st.text('Training Hyperparameters:')
       Hyperparams = st.json(ERL_PARAMS)
       st.text(f'Confs and Models saved to: {save_path}')
+      training_args = {
+      'start_date': TrainStartDate,
+      'end_date': TrainEndDate,
+      'ticker_list': ticker_list,
+      'data_source': 'alpaca',
+      'time_interval': TrainTradeInterval,
+      'technical_indicator_list': INDICATORS,
+      'drl_lib': AlgoLib,
+      'env': env,
+      'model_name': Algo.lower(),
+      'API_KEY': API_KEY,
+      'API_SECRET': API_SECRET,
+      'API_BASE_URL': API_BASE_URL,
+      'erl_params': ERL_PARAMS,
+      'cwd': save_path,  # current_working_dir
+      'wandb': False,
+      'break_step': 1e7
+      }
+      process = multiprocessing.Process(target=train_process, kwargs=training_args)
+      process.daemon = True  # let subprocess run on its own without blocking the main process
+      process.start()
+      st.text(f'Process ID for current run: {process.pid}')
+      # process.join()  # if join(), the main process will wait for the subprocess to finish before continuing
 
-      train(start_date=TrainStartDate,
-      end_date=TrainEndDate,
-      ticker_list=ticker_list,
-      data_source='alpaca',
-      time_interval=TrainTradeInterval,
-      technical_indicator_list=INDICATORS,
-      drl_lib=AlgoLib,
-      env=env,
-      model_name=Algo.lower(),
-      API_KEY=API_KEY,
-      API_SECRET=API_SECRET,
-      API_BASE_URL=API_BASE_URL,
-      erl_params=ERL_PARAMS,
-      cwd=save_path,  # current_working_dir
-      wandb=False,
-      break_step=1e7)
+      # train(start_date=TrainStartDate,
+      # end_date=TrainEndDate,
+      # ticker_list=ticker_list,
+      # data_source='alpaca',
+      # time_interval=TrainTradeInterval,
+      # technical_indicator_list=INDICATORS,
+      # drl_lib=AlgoLib,
+      # env=env,
+      # model_name=Algo.lower(),
+      # API_KEY=API_KEY,
+      # API_SECRET=API_SECRET,
+      # API_BASE_URL=API_BASE_URL,
+      # erl_params=ERL_PARAMS,
+      # cwd=save_path,  # current_working_dir
+      # wandb=False,
+      # break_step=1e7)
 
 
 st.subheader('BackTest', anchor=None)
