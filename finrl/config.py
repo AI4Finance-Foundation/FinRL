30bdc4593a127a6d2a5abc17a7c3c6b75030743f
AFTERaB <103228780+AFTERaB@users.noreply.github.com>
2023-03-16 18:39:13 -0400

    Update config.py

--- finrl/config.py
+++ finrl/config.py
@@ -70,7 +70,7 @@ TIME_ZONE_SELFDEFINED = "xxx"  # If neither of the above is your time zone, you 
 USE_TIME_ZONE_SELFDEFINED = 0  # 0 (default) or 1 (use the self defined)
 
 # parameters for data sources
-ALPACA_API_KEY = "xxx"  # your ALPACA_API_KEY
-ALPACA_API_SECRET = "xxx"  # your ALPACA_API_SECRET
+ALPACA_API_KEY = "PKAMN1K7V6A77K4IX65J"  # your ALPACA_API_KEY
+ALPACA_API_SECRET = "ekehU1CxrjYeNTDPW9KQx0QmTbCS0OcNJDxl3zTa"  # your ALPACA_API_SECRET
 ALPACA_API_BASE_URL = "https://paper-api.alpaca.markets"  # alpaca url
 BINANCE_BASE_URL = "https://data.binance.vision/"  # binance url
