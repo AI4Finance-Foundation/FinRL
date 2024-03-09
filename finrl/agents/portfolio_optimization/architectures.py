b3477bc3c8949c01af07a73db6585a0035209b45
Caio <csbc326@gmail.com>
2024-03-08 21:02:09 -0300

    Add GPM architecture

--- finrl/agents/portfolio_optimization/architectures.py
+++ finrl/agents/portfolio_optimization/architectures.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 import numpy as np
 import torch
 from torch import nn
+from torch_geometric.nn import RGCNConv, GCNConv, Sequential
+from torch_geometric.data import Batch, Data
 
 
 class EIIE(nn.Module):
@@ -259,3 +261,195 @@ class EI3(nn.Module):
         last_stocks = last_action[:, 1:].reshape((batch_size, 1, stocks, 1))
         cash_bias = last_action[:, 0].reshape((batch_size, 1, 1, 1))
         return last_stocks, cash_bias
+
+
+class GPM(nn.Module):
+    def __init__(
+        self,
+        num_nodes,
+        nodes_to_select,
+        initial_features=3,
+        k_short=3,
+        k_medium=21,
+        conv_mid_features=3,
+        conv_final_features=20,
+        time_window=50,
+        device="cpu",
+    ):
+        """GPM (Graph-based Portfolio Management) policy network initializer.
+
+        Args:
+            num_nodes: Number of nodes.
+            initial_features: Number of input features.
+            k_short: Size of short convolutional kernel.
+            k_medium: Size of medium convolutional kernel.
+            conv_mid_features: Size of intermediate convolutional channels.
+            conv_final_features: Size of final convolutional channels.
+            time_window: Size of time window used as agent's state.
+            device: Device in which the neural network will be run.
+
+        Note:
+            Reference article: https://doi.org/10.1016/j.neucom.2022.04.105.
+        """
+        super().__init__()
+        self.device = device
+
+        if isinstance(nodes_to_select, np.ndarray):
+            nodes_to_select = torch.from_numpy(nodes_to_select)
+        elif isinstance(nodes_to_select, list):
+            nodes_to_select = torch.tensor(nodes_to_select)
+        self.nodes_to_select = nodes_to_select
+
+        n_short = time_window - k_short + 1
+        n_medium = time_window - k_medium + 1
+        n_long = time_window
+
+        self.short_term = nn.Sequential(
+            nn.Conv2d(
+                in_channels=initial_features,
+                out_channels=conv_mid_features,
+                kernel_size=(1, k_short),
+            ),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels=conv_mid_features,
+                out_channels=conv_final_features,
+                kernel_size=(1, n_short),
+            ),
+            nn.ReLU(),
+        )
+
+        self.mid_term = nn.Sequential(
+            nn.Conv2d(
+                in_channels=3, out_channels=conv_mid_features, kernel_size=(1, k_medium)
+            ),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels=conv_mid_features,
+                out_channels=conv_final_features,
+                kernel_size=(1, n_medium),
+            ),
+            nn.ReLU(),
+        )
+
+        self.long_term = nn.Sequential(nn.MaxPool2d(kernel_size=(1, n_long)), nn.ReLU())
+
+        self.final_convolution = nn.Conv2d(
+            in_channels=2 * conv_final_features + initial_features + 1, # ERROR: FIX
+            out_channels=1,
+            kernel_size=(1, 1),
+        )
+
+        self.gcn = Sequential("x, edge_index", [
+            (GCNConv(num_nodes, num_nodes), "x, edge_index -> x"),
+            nn.LeakyReLU(),
+            (GCNConv(num_nodes, num_nodes), "x, edge_index -> x"),
+            nn.LeakyReLU(),
+        ] 
+        )
+
+        self.softmax = nn.Sequential(nn.Softmax(dim=-1))
+
+    def mu(self, observation, last_action, edge_index):
+        """Defines a most favorable action of this policy given input x.
+
+        Args:
+          observation: environment observation.
+          last_action: Last action performed by agent.
+          edge_index: Graph connectivity in COO format.
+
+        Returns:
+          Most favorable action.
+        """
+
+        if isinstance(observation, np.ndarray):
+            observation = torch.from_numpy(observation).to(self.device)
+        if isinstance(last_action, np.ndarray):
+            last_action = torch.from_numpy(last_action).to(self.device)
+
+        last_stocks, cash_bias = self._process_last_action(last_action)
+        cash_bias = torch.zeros_like(cash_bias).to(self.device)
+
+        short_features = self.short_term(observation.float())
+        medium_features = self.mid_term(observation.float())
+        long_features = self.long_term(observation.float())
+
+        temporal_features = torch.cat(
+            [last_stocks, short_features, medium_features, long_features], dim=1
+        ) # shape [N, feature_size, num_stocks, 1]
+
+        # add features to graph
+        graph_batch = self._create_graph_batch(temporal_features, edge_index)
+
+        # perform graph convolution
+        graph_features = self.gcn(graph_batch, edge_index) # shape [N, num_stocks, feature_size]
+        graph_features = torch.transpose(graph_features, 1, 2) # shape [N, feature_size, num_stocks]
+        graph_features = torch.unsqueeze(graph_features, 3) # shape [N, feature_size, num_stocks, 1]
+
+        # concatenate graph features and temporal features
+        features = torch.cat([temporal_features, graph_features], dim=1) # shape [N, 2 * feature_size, num_stocks, 1]
+
+        # perform selection
+        features = torch.index_select(features, dim=2, index=self.nodes_to_select) # shape [N, 2 * feature_size, portfolio_size, 1]
+
+        # final convolution 
+        output = self.final_convolution(features) # shape [N, 1, portfolio_size, 1]
+        output = torch.cat([cash_bias, output], dim=2) # shape [N, 1, portfolio_size + 1, 1]
+
+        # output shape must be [N, portfolio_size + 1] = [1, portfolio_size + 1], being N batch size
+        output = torch.squeeze(output, 3)
+        output = torch.squeeze(output, 1)  # shape [N, portfolio_size + 1]
+
+        output = self.softmax(output)
+
+        return output
+
+    def forward(self, observation, last_action, edge_index):
+        """Policy network's forward propagation.
+
+        Args:
+          observation: Environment observation (dictionary).
+          last_action: Last action performed by the agent.
+          edge_index: Graph connectivity in COO format.
+
+        Returns:
+          Action to be taken (numpy array).
+        """
+        mu = self.mu(observation, last_action, edge_index)
+        action = mu.cpu().detach().numpy().squeeze()
+        return action
+
+    def _process_last_action(self, last_action):
+        """Process the last action to retrieve cash bias and last stocks.
+
+        Args:
+          last_action: Last performed action.
+
+        Returns:
+          Last stocks and cash bias.
+        """
+        batch_size = last_action.shape[0]
+        stocks = last_action.shape[1] - 1
+        last_stocks = last_action[:, 1:].reshape((batch_size, 1, stocks, 1))
+        cash_bias = last_action[:, 0].reshape((batch_size, 1, 1, 1))
+        return last_stocks, cash_bias
+    
+    def _create_graph_batch(self, features, edge_index):
+        """Create a btach of graphs with the features
+        
+        Args:
+          features: Tensor of shape [batch_size, feature_size, num_stocks, 1]
+          graph: A graph with num_stocks nodes.
+          edge_index: Graph connectivity in COO format.
+        
+        Returns:
+          A batch of graphs with temporal features associated with each node.
+        """
+        batch_size = features.shape[0]
+        graphs = []
+        for i in range(batch_size):
+            x = features[i, :, :, 0] # shape [feature_size, num_stocks] -> CONFIRM
+            x = torch.transpose(x, 0, 1) # shape [num_stocks, feature_size]
+            new_graph = Data(x=x, edge_index=edge_index).to(self.device)
+            graphs.append(new_graph)
+        return Batch.from_data_list(graphs)
