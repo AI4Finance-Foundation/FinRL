c49db1dd91d0c4904426612168dd27efc2c8acae
Caio <csbc326@gmail.com>
2024-04-17 00:25:56 -0300

    Add groupby scaler

--- finrl/meta/preprocessor/preprocessors.py
+++ finrl/meta/preprocessor/preprocessors.py
@@ -6,6 +6,8 @@ from multiprocessing.sharedctypes import Value
 import numpy as np
 import pandas as pd
 from stockstats import StockDataFrame as Sdf
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import MinMaxScaler
 
 from finrl import config
 from finrl.meta.preprocessor.yahoodownloader import YahooDownloader
@@ -37,7 +39,63 @@ def convert_to_datetime(time):
     time_fmt = "%Y-%m-%dT%H:%M:%S"
     if isinstance(time, str):
         return datetime.datetime.strptime(time, time_fmt)
+    
 
+class GroupByScaler(BaseEstimator, TransformerMixin):
+    """Sklearn-like scaler that scales considering groups of data.
+
+    In the financial setting, this scale can be used to normalize a DataFrame
+    with time series of multiple tickers. The scaler will fit and transform
+    data for each ticker independently.
+    """
+    def __init__(self, by, scaler=MinMaxScaler, columns=None):
+        """Initializes GoupBy scaler.
+
+        Args:
+            by: Name of column that will be used to group.
+            scaler: Scikit-learn scaler class to be used.
+            columns: List of columns that will be scaled.
+        """
+        self.scalers = {} # dictionary with scalers
+        self.by = by
+        self.scaler = scaler
+        self.columns = columns
+
+    def fit(self, X, y = None):
+        """Fits the scaler to input data.
+
+        Args:
+            X: DataFrame to fit.
+            y: Not used.
+        
+        Returns:
+            Fitted GroupBy scaler.
+        """
+        # if columns aren't specified, considered all numeric columns
+        if self.columns is None:
+            self.columns = X.select_dtypes(exclude=['object']).columns
+        # fit one scaler for each group
+        for value in X[self.by].unique():
+            X_group = X.loc[X[self.by] == value, self.columns]
+            self.scalers[value] = self.scaler().fit(X_group)
+        return self
+
+    def transform(self, X, y = None):
+        """Transforms unscaled data.
+
+        Args:
+            X: DataFrame to transform.
+            y: Not used.
+        
+        Returns:
+            Transformed DataFrame.
+        """
+        # apply scaler for each group
+        X = X.copy()
+        for value in X[self.by].unique():
+            select_mask = X[self.by] == value
+            X.loc[select_mask, self.columns] = self.scalers[value].transform(X.loc[select_mask, self.columns])
+        return X
 
 class FeatureEngineer:
     """Provides methods for preprocessing the stock price data
