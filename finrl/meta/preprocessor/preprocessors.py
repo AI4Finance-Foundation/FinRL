8e621298b32427aa4483980511c92ff0b54910fd
Caio Souza <48871523+C4i0kun@users.noreply.github.com>
2024-04-21 05:09:29 -0300

    Add GroupBy Scaler (#1212)
    * Add graph convolutional layers parameter to GPM
    
    * Add groupby scaler
    
    * Format code
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Change groupby scaler default scaler
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update portfolio optimization example
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- finrl/meta/preprocessor/preprocessors.py
+++ finrl/meta/preprocessor/preprocessors.py
@@ -5,6 +5,9 @@ from multiprocessing.sharedctypes import Value
 
 import numpy as np
 import pandas as pd
+from sklearn.base import BaseEstimator
+from sklearn.base import TransformerMixin
+from sklearn.preprocessing import MaxAbsScaler
 from stockstats import StockDataFrame as Sdf
 
 from finrl import config
@@ -37,6 +40,68 @@ def convert_to_datetime(time):
     time_fmt = "%Y-%m-%dT%H:%M:%S"
     if isinstance(time, str):
         return datetime.datetime.strptime(time, time_fmt)
+
+
+class GroupByScaler(BaseEstimator, TransformerMixin):
+    """Sklearn-like scaler that scales considering groups of data.
+
+    In the financial setting, this scale can be used to normalize a DataFrame
+    with time series of multiple tickers. The scaler will fit and transform
+    data for each ticker independently.
+    """
+
+    def __init__(self, by, scaler=MaxAbsScaler, columns=None, scaler_kwargs=None):
+        """Initializes GoupBy scaler.
+
+        Args:
+            by: Name of column that will be used to group.
+            scaler: Scikit-learn scaler class to be used.
+            columns: List of columns that will be scaled.
+            scaler_kwargs: Keyword arguments for chosen scaler.
+        """
+        self.scalers = {}  # dictionary with scalers
+        self.by = by
+        self.scaler = scaler
+        self.columns = columns
+        self.scaler_kwargs = {} if scaler_kwargs is None else scaler_kwargs
+
+    def fit(self, X, y=None):
+        """Fits the scaler to input data.
+
+        Args:
+            X: DataFrame to fit.
+            y: Not used.
+
+        Returns:
+            Fitted GroupBy scaler.
+        """
+        # if columns aren't specified, considered all numeric columns
+        if self.columns is None:
+            self.columns = X.select_dtypes(exclude=["object"]).columns
+        # fit one scaler for each group
+        for value in X[self.by].unique():
+            X_group = X.loc[X[self.by] == value, self.columns]
+            self.scalers[value] = self.scaler(**self.scaler_kwargs).fit(X_group)
+        return self
+
+    def transform(self, X, y=None):
+        """Transforms unscaled data.
+
+        Args:
+            X: DataFrame to transform.
+            y: Not used.
+
+        Returns:
+            Transformed DataFrame.
+        """
+        # apply scaler for each group
+        X = X.copy()
+        for value in X[self.by].unique():
+            select_mask = X[self.by] == value
+            X.loc[select_mask, self.columns] = self.scalers[value].transform(
+                X.loc[select_mask, self.columns]
+            )
+        return X
 
 
 class FeatureEngineer:
