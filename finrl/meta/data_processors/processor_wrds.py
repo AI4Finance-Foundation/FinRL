6962e084620da1e5321212e38b6d99d6b5aeee1e
Ming Zhu <zhumingpassional@gmail.com>
2025-04-04 20:15:07 +0800

    revise examples

--- finrl/meta/data_processors/processor_wrds.py
+++ finrl/meta/data_processors/processor_wrds.py
@@ -41,7 +41,7 @@ class WrdsProcessor:
                 trading_days.append(str(day)[:10])
             return trading_days
 
-        def data_fetch_wrds(date="2021-05-01", stock_set=("AAPL"), time_interval=60):
+        def data_fetch_wrds(date, stock_set):
             # start_date, end_date should be in the same year
             current_date = datetime.datetime.strptime(date, "%Y-%m-%d")
             lib = "taqm_" + str(current_date.year)  # taqm_2021
