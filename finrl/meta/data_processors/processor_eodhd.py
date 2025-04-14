539beb74f6849428f38f2e0ea518fb9f8db5a2e7
aymeric75 <aymeric.barbin@hotmail.fr>
2025-04-14 15:05:47 +0200

    Update processor_eodhd.py

--- finrl/meta/data_processors/processor_eodhd.py
+++ finrl/meta/data_processors/processor_eodhd.py
@@ -1,1 +1,13 @@
+from __future__ import annotations
 
+import datetime
+
+import numpy as np
+import pandas as pd
+import pandas_market_calendars as tc
+import pytz
+
+
+class EodhdProcessor:
+    def __init__(self, if_offline=False):
+        
