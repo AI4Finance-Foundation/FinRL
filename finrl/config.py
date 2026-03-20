cc9f71e45036cb2a80bc11ec3d1c96d29909e2f4
ByFinTech <hongyang.yang@columbia.edu>
2026-03-20 16:39:56 +0800

    Update config.py

--- finrl/config.py
+++ finrl/config.py
@@ -7,14 +7,14 @@ TENSORBOARD_LOG_DIR = "tensorboard_log"
 RESULTS_DIR = "results"
 
 # date format: '%Y-%m-%d'
-TRAIN_START_DATE = "2014-01-06"  # bug fix: set Monday right, start date set 2014-01-01 ValueError: all the input array dimensions for the concatenation axis must match exactly, but along dimension 0, the array at index 0 has size 1658 and the array at index 1 has size 1657
-TRAIN_END_DATE = "2020-07-31"
+TRAIN_START_DATE = "2014-01-06"
+TRAIN_END_DATE = "2025-12-31"
 
-TEST_START_DATE = "2020-08-01"
-TEST_END_DATE = "2021-10-01"
+TEST_START_DATE = "2026-01-01"
+TEST_END_DATE = "2026-03-20"
 
-TRADE_START_DATE = "2021-11-01"
-TRADE_END_DATE = "2021-12-01"
+TRADE_START_DATE = "2026-01-01"
+TRADE_END_DATE = "2026-03-20"
 
 # stockstats technical indicator column names
 # check https://pypi.org/project/stockstats/ for different names
